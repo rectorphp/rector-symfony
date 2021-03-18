3d5f34577a786152841c65d4be3a7ae1e6919d1b
TomasVotruba <tomas.vot@gmail.com>
2021-03-18 20:36:10 +0100

    decopule ServiceMapFactory

--- config/config.php
+++ config/config.php
@@ -8,8 +8,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->defaults()
         ->public()
-        ->autowire();
+        ->autowire()
+        ->autoconfigure();
 
     $services->load('Rector\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/*/{Rector,ValueObject}/*']);
+        ->exclude([__DIR__ . '/../src/*/Rector/*', __DIR__ . '/../src/*/ValueObject/*']);
 };
