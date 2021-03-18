c3068da0b55a11dbee095ea9632d5ea0c4b561eb
TomasVotruba <tomas.vot@gmail.com>
2021-03-18 18:41:28 +0100

    add sets

---
+++ src/Set/SymfonySetList.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Set;
+
+final class SymfonySetList
+{
+    /**
+     * @var string
+     */
+    public const SYMFONY_26 = __DIR__ . '/../../config/sets/symfony26.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_28 = __DIR__ . '/../../config/sets/symfony28.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_30 = __DIR__ . '/../../config/sets/symfony30.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_31 = __DIR__ . '/../../config/sets/symfony31.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_32 = __DIR__ . '/../../config/sets/symfony32.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_33 = __DIR__ . '/../../config/sets/symfony33.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_34 = __DIR__ . '/../../config/sets/symfony34.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_40 = __DIR__ . '/../../config/sets/symfony40.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_41 = __DIR__ . '/../../config/sets/symfony41.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_42 = __DIR__ . '/../../config/sets/symfony42.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_43 = __DIR__ . '/../../config/sets/symfony43.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_44 = __DIR__ . '/../../config/sets/symfony44.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_50 = __DIR__ . '/../../config/sets/symfony50.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_50_TYPES = __DIR__ . '/../../config/sets/symfony50-types.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_52 = __DIR__ . '/../../config/sets/symfony52.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_CODE_QUALITY = __DIR__ . '/../../config/sets/symfony-code-quality.php';
+
+    /**
+     * @var string
+     */
+    public const SYMFONY_CONSTRUCTOR_INJECTION = __DIR__ . '/../../config/sets/symfony-constructor-injection.php';
+}
