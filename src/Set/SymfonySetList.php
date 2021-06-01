1746319f7c776f7ab97fc5eb79dd3a6e6011409c
Tomas Votruba <tomas.vot@gmail.com>
2021-06-01 12:00:35 +0200

    add Symfony Validator Attributes set (#16)

--- src/Set/SymfonySetList.php
+++ src/Set/SymfonySetList.php
@@ -86,6 +86,11 @@ final class SymfonySetList implements SetListInterface
     /**
      * @var string
      */
+    public const SYMFONY_52_VALIDATOR_ATTRIBUTES = __DIR__ . '/../../config/sets/symfony/symfony52-validator-attributes.php';
+
+    /**
+     * @var string
+     */
     public const SYMFONY_CODE_QUALITY = __DIR__ . '/../../config/sets/symfony/symfony-code-quality.php';
 
     /**
