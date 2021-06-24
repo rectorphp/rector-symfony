d8dd561ab9dc32372c173872d4e2cdde1d3e2381
Tomas Votruba <tomas.vot@gmail.com>
2021-06-24 18:05:17 +0200

    fixup! [Attribute] Remove ExtractAttributeRouteNameConstantsRector, too opinionated and requires global context

--- config/sets/symfony/symfony-code-quality.php
+++ config/sets/symfony/symfony-code-quality.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\Symfony\Rector\Attribute\ExtractAttributeRouteNameConstantsRector;
 use Rector\Symfony\Rector\BinaryOp\ResponseStatusCodeRector;
 use Rector\Symfony\Rector\Class_\EventListenerToEventSubscriberRector;
 use Rector\Symfony\Rector\Class_\MakeCommandLazyRector;
@@ -13,5 +12,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(ResponseStatusCodeRector::class);
     $services->set(MakeCommandLazyRector::class);
     $services->set(EventListenerToEventSubscriberRector::class);
-    $services->set(ExtractAttributeRouteNameConstantsRector::class);
 };
