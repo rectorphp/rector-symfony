30dc63538defe3bafd0626662d4b8d3524a0f352
Bl00D4NGEL <kuhlesdominik@gmx.de>
2021-08-22 14:36:01 +0200

    cleanup of SwiftCreateMessageToNewEmailRector

--- src/Rector/MethodCall/SwiftCreateMessageToNewEmailRector.php
+++ src/Rector/MethodCall/SwiftCreateMessageToNewEmailRector.php
@@ -6,7 +6,9 @@ declare(strict_types=1);
 namespace Rector\Symfony\Rector\MethodCall;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
 use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 final class SwiftCreateMessageToNewEmailRector extends AbstractRector
@@ -14,20 +16,40 @@ final class SwiftCreateMessageToNewEmailRector extends AbstractRector
 
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('', []);
-    }
+        return new RuleDefinition(
+            'Changes SwiftMailer\'s createMessage into a new Symfony\Component\Mime\Email',
+            [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+public function createMessage()
+{
+    $email = $this->swift->createMessage('message');
+}
+CODE_SAMPLE,
+                <<<'CODE_SAMPLE'
+public function createMessage()
+{
+    $email = new \Symfony\Component\Mime\Email();
+}
+CODE_SAMPLE
 
-    public function getNodeTypes(): array
-    {
-        return [
-            Node\Expr\MethodCall::class
-        ];
+
+            )
+        ]);
     }
 
     /**
-     * @var Node\Expr\MethodCall $node
+     * @return array<class-string<Node>>
      */
-    public function refactor(Node $node)
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param MethodCall $node
+     */
+    public function refactor(Node $node): ?Node
     {
         if (!$this->isName($node->name, 'createMessage')) {
             return null;
