9241a02033fb03cc702400b92c2cb5627c87a10f
Tomas Votruba <tomas.vot@gmail.com>
2021-10-23 12:45:23 +0200

    Add SymfonyLevelSetList (#44)

---
+++ config/sets/symfony/level/up-to-symfony-30.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(SymfonySetList::SYMFONY_30);
+    $containerConfigurator->import(SymfonyLevelSetList::UP_TO_SYMFONY_28);
+};
