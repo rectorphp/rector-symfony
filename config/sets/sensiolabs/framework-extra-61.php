ca6ff9cb379326c96cf9fde256c76f97006d0d85
Tomas Votruba <tomas.vot@gmail.com>
2021-12-06 23:55:06 +0300

    fixup! Add FrameworkExtraBundle with attributes set

--- config/sets/sensiolabs/framework-extra-61.php
+++ config/sets/sensiolabs/framework-extra-61.php
@@ -2,15 +2,15 @@
 
 declare(strict_types=1);
 
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
-use Rector\Renaming\Rector\Name\RenameClassRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     // @see https://github.com/sensiolabs/SensioFrameworkExtraBundle/pull/707
-    $services->set(\Rector\Php80\Rector\Class_\AnnotationToAttributeRector::class)
+    $services->set(AnnotationToAttributeRector::class)
         ->configure([
             new AnnotationToAttribute('Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache'),
             new AnnotationToAttribute('Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity'),
