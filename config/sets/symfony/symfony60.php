e77723842df081ee5ad7f631afbb6d84110d6069
Tomas Votruba <tomas.vot@gmail.com>
2021-12-09 08:44:04 +0100

    add basic Loader and Kernel types

--- config/sets/symfony/symfony60.php
+++ config/sets/symfony/symfony60.php
@@ -2,8 +2,13 @@
 
 declare(strict_types=1);
 
+use PHPStan\Type\IterableType;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\ObjectType;
 use Rector\Symfony\Set\SymfonySetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\TypeDeclaration\ValueObject\AddParamTypeDeclaration;
 use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -16,5 +21,33 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     // @see https://github.com/symfony/symfony/pull/42064
     $services->set(AddReturnTypeDeclarationRector::class)
-        ->configure([new AddReturnTypeDeclaration()]);
+        ->configure([
+            new AddReturnTypeDeclaration(
+                'Symfony\Component\Config\Loader\LoaderInterface',
+                'load',
+                new MixedType(),
+            ),
+            new AddReturnTypeDeclaration('Symfony\Component\Config\Loader\Loader', 'import', new MixedType(),),
+            new AddReturnTypeDeclaration(
+                'Symfony\Component\HttpKernel\KernelInterface',
+                'registerBundles',
+                new IterableType(),
+            ),
+        ]);
+
+    $services->set(AddParamTypeDeclarationRector::class)
+        ->configure([
+            new AddParamTypeDeclaration(
+                'Symfony\Component\Config\Loader\LoaderInterface',
+                'load',
+                0,
+                new MixedType(),
+            ),
+            new AddParamTypeDeclaration(
+                'Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait',
+                'configureRoutes',
+                0,
+                new ObjectType('Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator'),
+            ),
+        ]);
 };
