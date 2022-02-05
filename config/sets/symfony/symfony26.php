472f1621e86e65283a3a170f18398e52edd62c0e
Tomas Votruba <tomas.vot@gmail.com>
2022-02-05 11:58:39 +0100

    remove AddFlashRector, very specific rule with low support in Symfony 2.6

--- config/sets/symfony/symfony26.php
+++ config/sets/symfony/symfony26.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\Symfony\Rector\MethodCall\AddFlashRector;
 use Rector\Symfony\Rector\MethodCall\RedirectToRouteRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -10,6 +9,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(RedirectToRouteRector::class);
-
-    $services->set(AddFlashRector::class);
 };
