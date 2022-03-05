d261a75a5953189e01a35034895ef239a00c8aae
Tomas Votruba <tomas.vot@gmail.com>
2022-03-05 09:56:39 +0000

    Fix exluding of renamed classes in Twig underscore set (#116)

---
+++ tests/Set/TwigExtensionNamespace/TwigExtensionNamespaceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Tests\Set\TwigExtensionNamespace;
+
+use Rector\Symfony\Set\TwigSetList;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class TwigExtensionNamespaceTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    /**
+     * @return \Iterator<SmartFileInfo>
+     */
+    public function provideData(): \Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return TwigSetList::TWIG_UNDERSCORE_TO_NAMESPACE;
+    }
+}
