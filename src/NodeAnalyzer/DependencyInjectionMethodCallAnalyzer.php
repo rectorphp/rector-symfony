826c6cc35487c018b7af7605a6c1b8874226aa0b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-03-06 20:04:12 +0700

    [Symfony] Handle property exists on ContainerGetToConstructorInjectionRector (#117)
    * [Symfony] Handle property exists on ContainerGetToConstructorInjectionRector
    
    * [Symfony] Handle property exists on ContainerGetToConstructorInjectionRector
    
    * [ci-review] Rector Rectify
    
    * use original property name for init before add count value
    
    * [ci-review] Rector Rectify
    
    * final touch: clean up
    
    * final touch: clean up
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/NodeAnalyzer/DependencyInjectionMethodCallAnalyzer.php
+++ src/NodeAnalyzer/DependencyInjectionMethodCallAnalyzer.php
@@ -7,10 +7,13 @@ namespace Rector\Symfony\NodeAnalyzer;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Property;
 use PHPStan\Type\ObjectType;
 use Rector\Core\PhpParser\Node\BetterNodeFinder;
 use Rector\Core\PhpParser\Node\NodeFactory;
 use Rector\Naming\Naming\PropertyNaming;
+use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\Php80\NodeAnalyzer\PromotedPropertyResolver;
 use Rector\PostRector\Collector\PropertyToAddCollector;
 use Rector\PostRector\ValueObject\PropertyMetadata;
 
@@ -22,6 +25,8 @@ final class DependencyInjectionMethodCallAnalyzer
         private readonly NodeFactory $nodeFactory,
         private readonly PropertyToAddCollector $propertyToAddCollector,
         private readonly BetterNodeFinder $betterNodeFinder,
+        private readonly PromotedPropertyResolver $promotedPropertyResolver,
+        private readonly NodeNameResolver $nodeNameResolver
     ) {
     }
 
@@ -38,10 +43,40 @@ final class DependencyInjectionMethodCallAnalyzer
         }
 
         $propertyName = $this->propertyNaming->fqnToVariableName($serviceType);
+        $propertyName = $this->resolveNewPropertyNameWhenExists($class, $propertyName, $propertyName);
 
         $propertyMetadata = new PropertyMetadata($propertyName, $serviceType, Class_::MODIFIER_PRIVATE);
         $this->propertyToAddCollector->addPropertyToClass($class, $propertyMetadata);
 
         return $this->nodeFactory->createPropertyFetch('this', $propertyName);
+    }
+
+    private function resolveNewPropertyNameWhenExists(
+        Class_ $class,
+        string $originalPropertyName,
+        string $propertyName,
+        int $count = 1
+    ): string {
+        $promotedPropertyParams = $this->promotedPropertyResolver->resolveFromClass($class);
+        foreach ($promotedPropertyParams as $promotedPropertyParam) {
+            if ($this->nodeNameResolver->isName($promotedPropertyParam->var, $propertyName)) {
+                $propertyName = $this->resolveIncrementPropertyName($originalPropertyName, $count);
+                return $this->resolveNewPropertyNameWhenExists($class, $originalPropertyName, $propertyName, $count);
+            }
+        }
+
+        $property = $class->getProperty($propertyName);
+        if (! $property instanceof Property) {
+            return $propertyName;
+        }
+
+        $propertyName = $this->resolveIncrementPropertyName($originalPropertyName, $count);
+        return $this->resolveNewPropertyNameWhenExists($class, $originalPropertyName, $propertyName, $count);
+    }
+
+    private function resolveIncrementPropertyName(string $originalPropertyName, int $count): string
+    {
+        ++$count;
+        return $originalPropertyName . $count;
     }
 }
