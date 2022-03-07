6cc95d15525e4b6a086114bb37dc7bc938f8863b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-03-07 16:59:21 +0700

    [Symfony 4.4] Add AuthorizationCheckerIsGrantedExtractorRector (#119)

--- config/sets/symfony/symfony44.php
+++ config/sets/symfony/symfony44.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Symfony\Rector\ClassMethod\ConsoleExecuteReturnIntRector;
+use Rector\Symfony\Rector\MethodCall\AuthorizationCheckerIsGrantedExtractorRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 # https://github.com/symfony/symfony/blob/4.4/UPGRADE-4.4.md
@@ -12,4 +13,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     # https://github.com/symfony/symfony/pull/33775
     $services->set(ConsoleExecuteReturnIntRector::class);
+
+    # https://github.com/symfony/symfony/blob/4.4/UPGRADE-4.4.md#security
+    $services->set(AuthorizationCheckerIsGrantedExtractorRector::class);
 };
