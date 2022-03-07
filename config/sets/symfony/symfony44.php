412882566c8edb0a1d13265e365b3a553f59abc9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-03-07 09:50:15 +0700

    register

--- config/sets/symfony/symfony44.php
+++ config/sets/symfony/symfony44.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Symfony\Rector\ClassMethod\ConsoleExecuteReturnIntRector;
+use Rector\Symfony\Rector\MethodCall\AuthorizationCheckerIsGrantedExtractorRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 # https://github.com/symfony/symfony/blob/4.4/UPGRADE-4.4.md
