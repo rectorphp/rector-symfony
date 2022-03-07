111d733662ddae8275aaa3f85ba5c229661e04f2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-03-07 09:47:16 +0700

    [Symfony 4.4] Add AuthorizationCheckerIsGrantedExtractorRector

---
+++ tests/Rector/MethodCall/AuthorizationCheckerIsGrantedExtractorRector/Source/DifferentClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rector\Symfony\Tests\Rector\MethodCall\AuthorizationCheckerIsGrantedExtractorRector\Source;
+
+class DifferentClass
+{
+    public function isGranted($args)
+    {
+    }
+
+    public function differentMethod($args)
+    {
+    }
+}
