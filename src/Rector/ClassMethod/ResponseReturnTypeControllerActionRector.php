5ba000274809542dac65856e4fe9e4f91487b580
Tomas Votruba <tomas.vot@gmail.com>
2022-03-11 16:22:36 +0100

    Add ResponseReturnTypeControllerActionRector to code-quality set (#124)

---
+++ src/Rector/ClassMethod/ResponseReturnTypeControllerActionRector.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Rector\ClassMethod;
+
+use PhpParser\Node;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Stmt\ClassMethod;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Doctrine\NodeAnalyzer\AttrinationFinder;
+use Rector\Symfony\TypeAnalyzer\ControllerAnalyzer;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Symfony\Tests\Rector\ClassMethod\ResponseReturnTypeControllerActionRector\ResponseReturnTypeControllerActionRectorTest
+ */
+final class ResponseReturnTypeControllerActionRector extends AbstractRector
+{
+    public function __construct(
+        private readonly ControllerAnalyzer $controllerAnalyzer,
+        private readonly AttrinationFinder $attrinationFinder
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Add Response object return type to controller actions', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class SomeController extends AbstractController
+{
+    #[Route]
+    public function detail()
+    {
+        return $this->render('some_template');
+    }
+}
+CODE_SAMPLE
+
+                ,
+                <<<'CODE_SAMPLE'
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class SomeController extends AbstractController
+{
+    #[Route]
+    public function detail(): Response
+    {
+        return $this->render('some_template');
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    /**
+     * @param ClassMethod $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->isPublic()) {
+            return null;
+        }
+
+        if (! $this->controllerAnalyzer->isInsideController($node)) {
+            return null;
+        }
+
+        if ($node->returnType !== null) {
+            return null;
+        }
+
+        if (! $this->attrinationFinder->hasByOne($node, 'Symfony\Component\Routing\Annotation\Route')) {
+            return null;
+        }
+
+        $node->returnType = new FullyQualified('Symfony\Component\HttpFoundation\Response');
+        return $node;
+    }
+}
--- tests/Rector/ClassMethod/ResponseReturnTypeControllerActionRector/config/configured_rule.php
+++ tests/Rector/ClassMethod/ResponseReturnTypeControllerActionRector/config/configured_rule.php
@@ -2,16 +2,12 @@
 
 declare(strict_types=1);
 
-use Rector\Symfony\Rector\BinaryOp\ResponseStatusCodeRector;
-use Rector\Symfony\Rector\Class_\EventListenerToEventSubscriberRector;
-use Rector\Symfony\Rector\Class_\MakeCommandLazyRector;
 use Rector\Symfony\Rector\ClassMethod\ResponseReturnTypeControllerActionRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../../../../../config/config.php');
+
     $services = $containerConfigurator->services();
-    $services->set(ResponseStatusCodeRector::class);
-    $services->set(MakeCommandLazyRector::class);
-    $services->set(EventListenerToEventSubscriberRector::class);
     $services->set(ResponseReturnTypeControllerActionRector::class);
 };
