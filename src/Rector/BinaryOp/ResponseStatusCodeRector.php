43e2aff02548dfce75f95120a165dc8e31c3d75e
Tomas Votruba <tomas.vot@gmail.com>
2022-03-14 14:09:59 +0100

    fix cs

--- src/Rector/BinaryOp/ResponseStatusCodeRector.php
+++ src/Rector/BinaryOp/ResponseStatusCodeRector.php
@@ -6,7 +6,6 @@ namespace Rector\Symfony\Rector\BinaryOp;
 
 use PhpParser\Node;
 use PhpParser\Node\Arg;
-use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\BinaryOp;
 use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\MethodCall;
