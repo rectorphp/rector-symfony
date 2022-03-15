cae43bf7865f01b8ed2c63c5c031e3f4e1c68517
Alessandro Lai <alessandro.lai85@gmail.com>
2022-03-15 12:04:02 +0100

    Add set to migrate JMS annotations to attributes

--- src/Set/JMSSetList.php
+++ src/Set/JMSSetList.php
@@ -12,4 +12,9 @@ final class JMSSetList implements SetListInterface
      * @var string
      */
     final public const REMOVE_JMS_INJECT = __DIR__ . '/../../config/sets/jms/remove-jms-inject.php';
+
+    /**
+     * @var string
+     */
+    final public const ANNOTATIONS_TO_ATTRIBUTES = __DIR__ . '/../../config/sets/jms/annotations-to-attributes.php';
 }
