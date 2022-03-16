d0a6487e51d4959f4d2d6e0b3e8fa541505309ce
Tomas Votruba <tomas.vot@gmail.com>
2022-03-16 11:31:10 +0100

    [Symfony 2.8] Add type support to FormTypeInstanceToClassConstRector (#133)

---
+++ src/NodeAnalyzer/FormInstanceToFormClassConstFetchConverter.php
@@ -0,0 +1,147 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\NodeAnalyzer;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Scalar\String_;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\TypeWithClassName;
+use Rector\Core\PhpParser\Node\NodeFactory;
+use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\NodeTypeResolver\NodeTypeResolver;
+use ReflectionMethod;
+
+final class FormInstanceToFormClassConstFetchConverter
+{
+    public function __construct(
+        private readonly ReflectionProvider $reflectionProvider,
+        private readonly NodeFactory $nodeFactory,
+        private readonly NodeNameResolver $nodeNameResolver,
+        private readonly NodeTypeResolver $nodeTypeResolver
+    ) {
+    }
+
+    public function processNewInstance(MethodCall $methodCall, int $position, int $optionsPosition): ?Node
+    {
+        $args = $methodCall->getArgs();
+        if (! isset($args[$position])) {
+            return null;
+        }
+
+        $argValue = $args[$position]->value;
+
+        $formClassName = $this->resolveFormClassName($argValue);
+        if ($formClassName === null) {
+            return null;
+        }
+
+        if ($argValue instanceof New_ && $argValue->args !== []) {
+            $methodCall = $this->moveArgumentsToOptions(
+                $methodCall,
+                $position,
+                $optionsPosition,
+                $formClassName,
+                $argValue->getArgs()
+            );
+            if (! $methodCall instanceof MethodCall) {
+                return null;
+            }
+        }
+
+        $currentArg = $methodCall->getArgs()[$position];
+
+        $classConstReference = $this->nodeFactory->createClassConstReference($formClassName);
+        $currentArg->value = $classConstReference;
+
+        return $methodCall;
+    }
+
+    /**
+     * @param Arg[] $argNodes
+     */
+    private function moveArgumentsToOptions(
+        MethodCall $methodCall,
+        int $position,
+        int $optionsPosition,
+        string $className,
+        array $argNodes
+    ): ?MethodCall {
+        $namesToArgs = $this->resolveNamesToArgs($className, $argNodes);
+
+        // set default data in between
+        if ($position + 1 !== $optionsPosition && ! isset($methodCall->args[$position + 1])) {
+            $methodCall->args[$position + 1] = new Arg($this->nodeFactory->createNull());
+        }
+
+        // @todo decopule and name, so I know what it is
+        if (! isset($methodCall->args[$optionsPosition])) {
+            $array = new Array_();
+            foreach ($namesToArgs as $name => $arg) {
+                $array->items[] = new ArrayItem($arg->value, new String_($name));
+            }
+
+            $methodCall->args[$optionsPosition] = new Arg($array);
+        }
+
+        if (! $this->reflectionProvider->hasClass($className)) {
+            return null;
+        }
+
+        $formTypeClassReflection = $this->reflectionProvider->getClass($className);
+        if (! $formTypeClassReflection->hasConstructor()) {
+            return null;
+        }
+
+        // nothing we can do, out of scope
+        return $methodCall;
+    }
+
+    /**
+     * @param Arg[] $args
+     * @return array<string, Arg>
+     */
+    private function resolveNamesToArgs(string $className, array $args): array
+    {
+        if (! $this->reflectionProvider->hasClass($className)) {
+            return [];
+        }
+
+        $classReflection = $this->reflectionProvider->getClass($className);
+        $reflectionClass = $classReflection->getNativeReflection();
+
+        $constructorReflectionMethod = $reflectionClass->getConstructor();
+        if (! $constructorReflectionMethod instanceof ReflectionMethod) {
+            return [];
+        }
+
+        $namesToArgs = [];
+        foreach ($constructorReflectionMethod->getParameters() as $position => $reflectionParameter) {
+            $namesToArgs[$reflectionParameter->getName()] = $args[$position];
+        }
+
+        return $namesToArgs;
+    }
+
+    private function resolveFormClassName(Expr $expr): ?string
+    {
+        if ($expr instanceof New_) {
+            // we can only process direct name
+            return $this->nodeNameResolver->getName($expr->class);
+        }
+
+        $exprType = $this->nodeTypeResolver->getType($expr);
+        if ($exprType instanceof TypeWithClassName) {
+            return $exprType->getClassName();
+        }
+
+        return null;
+    }
+}
--- src/Rector/MethodCall/FormTypeInstanceToClassConstRector.php
+++ src/Rector/MethodCall/FormTypeInstanceToClassConstRector.php
@@ -5,21 +5,17 @@ declare(strict_types=1);
 namespace Rector\Symfony\Rector\MethodCall;
 
 use PhpParser\Node;
-use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Name;
 use PhpParser\Node\Param;
-use PhpParser\Node\Scalar\String_;
-use PHPStan\Reflection\ReflectionProvider;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Symfony\NodeAnalyzer\FormAddMethodCallAnalyzer;
 use Rector\Symfony\NodeAnalyzer\FormCollectionAnalyzer;
+use Rector\Symfony\NodeAnalyzer\FormInstanceToFormClassConstFetchConverter;
 use Rector\Symfony\NodeAnalyzer\FormOptionsArrayMatcher;
 use Rector\Symfony\TypeAnalyzer\ControllerAnalyzer;
-use ReflectionMethod;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -37,7 +33,7 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 final class FormTypeInstanceToClassConstRector extends AbstractRector
 {
     public function __construct(
-        private readonly ReflectionProvider $reflectionProvider,
+        private readonly FormInstanceToFormClassConstFetchConverter $formInstanceToFormClassConstFetchConverter,
         private readonly FormAddMethodCallAnalyzer $formAddMethodCallAnalyzer,
         private readonly FormOptionsArrayMatcher $formOptionsArrayMatcher,
         private readonly FormCollectionAnalyzer $formCollectionAnalyzer,
@@ -52,21 +48,25 @@ final class FormTypeInstanceToClassConstRector extends AbstractRector
             [
                 new CodeSample(
                     <<<'CODE_SAMPLE'
-class SomeController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+final class SomeController extends Controller
 {
     public function action()
     {
-        $form = $this->createForm(new TeamType, $entity);
+        $form = $this->createForm(new TeamType);
     }
 }
 CODE_SAMPLE
                     ,
                     <<<'CODE_SAMPLE'
-class SomeController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+final class SomeController extends Controller
 {
     public function action()
     {
-        $form = $this->createForm(TeamType::class, $entity);
+        $form = $this->createForm(TeamType::class);
     }
 }
 CODE_SAMPLE
@@ -89,7 +89,7 @@ CODE_SAMPLE
     public function refactor(Node $node): ?Node
     {
         if ($this->controllerAnalyzer->isController($node->var) && $this->isName($node->name, 'createForm')) {
-            return $this->processNewInstance($node, 0, 2);
+            return $this->formInstanceToFormClassConstFetchConverter->processNewInstance($node, 0, 2);
         }
 
         if (! $this->formAddMethodCallAnalyzer->isMatching($node)) {
@@ -101,45 +101,7 @@ CODE_SAMPLE
             $this->refactorCollectionOptions($node);
         }
 
-        return $this->processNewInstance($node, 1, 2);
-    }
-
-    private function processNewInstance(MethodCall $methodCall, int $position, int $optionsPosition): ?Node
-    {
-        if (! isset($methodCall->args[$position])) {
-            return null;
-        }
-
-        $argValue = $methodCall->getArgs()[$position]->value;
-        if (! $argValue instanceof New_) {
-            return null;
-        }
-
-        // we can only process direct name
-        if (! $argValue->class instanceof Name) {
-            return null;
-        }
-
-        if ($argValue->args !== []) {
-            $methodCall = $this->moveArgumentsToOptions(
-                $methodCall,
-                $position,
-                $optionsPosition,
-                $argValue->class->toString(),
-                $argValue->getArgs()
-            );
-
-            if (! $methodCall instanceof MethodCall) {
-                return null;
-            }
-        }
-
-        $currentArg = $methodCall->getArgs()[$position];
-
-        $classConstReference = $this->nodeFactory->createClassConstReference($argValue->class->toString());
-        $currentArg->value = $classConstReference;
-
-        return $methodCall;
+        return $this->formInstanceToFormClassConstFetchConverter->processNewInstance($node, 1, 2);
     }
 
     private function refactorCollectionOptions(MethodCall $methodCall): void
@@ -174,71 +136,5 @@ CODE_SAMPLE
 
             $arrayItem->value = $this->nodeFactory->createClassConstReference($newClass->toString());
         }
-    }
-
-    /**
-     * @param Arg[] $argNodes
-     */
-    private function moveArgumentsToOptions(
-        MethodCall $methodCall,
-        int $position,
-        int $optionsPosition,
-        string $className,
-        array $argNodes
-    ): ?MethodCall {
-        $namesToArgs = $this->resolveNamesToArgs($className, $argNodes);
-
-        // set default data in between
-        if ($position + 1 !== $optionsPosition && ! isset($methodCall->args[$position + 1])) {
-            $methodCall->args[$position + 1] = new Arg($this->nodeFactory->createNull());
-        }
-
-        // @todo decopule and name, so I know what it is
-        if (! isset($methodCall->args[$optionsPosition])) {
-            $array = new Array_();
-            foreach ($namesToArgs as $name => $arg) {
-                $array->items[] = new ArrayItem($arg->value, new String_($name));
-            }
-
-            $methodCall->args[$optionsPosition] = new Arg($array);
-        }
-
-        if (! $this->reflectionProvider->hasClass($className)) {
-            return null;
-        }
-
-        $formTypeClassReflection = $this->reflectionProvider->getClass($className);
-        if (! $formTypeClassReflection->hasConstructor()) {
-            return null;
-        }
-
-        // nothing we can do, out of scope
-        return $methodCall;
-    }
-
-    /**
-     * @param Arg[] $args
-     * @return array<string, Arg>
-     */
-    private function resolveNamesToArgs(string $className, array $args): array
-    {
-        if (! $this->reflectionProvider->hasClass($className)) {
-            return [];
-        }
-
-        $classReflection = $this->reflectionProvider->getClass($className);
-        $reflectionClass = $classReflection->getNativeReflection();
-
-        $constructorReflectionMethod = $reflectionClass->getConstructor();
-        if (! $constructorReflectionMethod instanceof ReflectionMethod) {
-            return [];
-        }
-
-        $namesToArgs = [];
-        foreach ($constructorReflectionMethod->getParameters() as $position => $reflectionParameter) {
-            $namesToArgs[$reflectionParameter->getName()] = $args[$position];
-        }
-
-        return $namesToArgs;
     }
 }
