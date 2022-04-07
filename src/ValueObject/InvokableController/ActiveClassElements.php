505279f6b93d1758332e6dd7a76f5041f65619bb
Tomas Votruba <tomas.vot@gmail.com>
2022-04-07 14:00:06 +0200

    add stmt removal

--- src/ValueObject/InvokableController/ActiveClassElements.php
+++ src/ValueObject/InvokableController/ActiveClassElements.php
@@ -4,23 +4,18 @@ declare(strict_types=1);
 
 namespace Rector\Symfony\ValueObject\InvokableController;
 
-use PhpParser\Node\Stmt\Property;
-
 final class ActiveClassElements
 {
     /**
      * @param string[] $propertyNames
      */
     public function __construct(
-        private array $propertyNames
+        private readonly array $propertyNames
     ) {
     }
 
-    public function hasProperty(Property $property): bool
+    public function hasPropertyName(string $propertyFetchName): bool
     {
-        $onlyProperty = $property->props[0];
-        $propertyName = $onlyProperty->name->toString();
-
-        return in_array($propertyName, $this->propertyNames, true);
+        return in_array($propertyFetchName, $this->propertyNames, true);
     }
 }
