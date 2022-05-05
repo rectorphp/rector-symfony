21eb055d5ecec11b6a4bbc62f589032734fcf265
Stephan Vierkant <stephan@vierkant.net>
2022-05-05 14:02:00 +0200

    Add CommandPropertyToAttributeRector

---
+++ src/Rector/Class_/CommandPropertyToAttributeRector.php
@@ -0,0 +1,146 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\AttributeGroup;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Property;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\ObjectType;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
+use Rector\PhpAttribute\Printer\PhpAttributeGroupFactory;
+use Rector\VersionBonding\Contract\MinPhpVersionInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see https://symfony.com/doc/current/console.html#registering-the-command
+ *
+ * @see \Rector\Symfony\Tests\Rector\Class_\CommandPropertyToAttributeRector\CommandPropertyToAttributeRectorTest
+ */
+final class CommandPropertyToAttributeRector extends AbstractRector implements MinPhpVersionInterface
+{
+    private const ATTRIBUTE = 'Symfony\Component\Console\Attribute\AsCommand';
+
+    public function __construct(
+        private readonly PhpAttributeGroupFactory $phpAttributeGroupFactory,
+        private readonly PhpAttributeAnalyzer $phpAttributeAnalyzer,
+        private readonly ReflectionProvider $reflectionProvider
+    ) {
+    }
+
+    public function provideMinPhpVersion(): int
+    {
+        return PhpVersionFeature::ATTRIBUTES;
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Add Symfony\Component\Console\Attribute\AsCommand to Symfony Commands and remove the deprecated properties',
+            [
+                new CodeSample(<<<'CODE_SAMPLE'
+class SunshineCommand extends \Symfony\Component\Console\Command\Command
+{
+    /** @var string|null */
+    public static $defaultName = 'sunshine';
+}
+CODE_SAMPLE
+                , <<<'CODE_SAMPLE'
+#[\Symfony\Component\Console\Attribute\AsCommand('sunshine')]
+class SunshineCommand extends \Symfony\Component\Console\Command\Command
+{
+}
+CODE_SAMPLE),
+
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Class_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isObjectType($node, new ObjectType('Symfony\\Component\\Console\\Command\\Command'))) {
+            return null;
+        }
+
+        if (! $this->reflectionProvider->hasClass(self::ATTRIBUTE)) {
+            return null;
+        }
+
+        if ($this->phpAttributeAnalyzer->hasPhpAttribute($node, self::ATTRIBUTE)) {
+            return null;
+        }
+
+        $defaultName = null;
+        $property = $node->getProperty('defaultName');
+        if ($property instanceof Property) {
+            $defaultName = $this->getValueFromProperty($property);
+
+            if ($defaultName !== null) {
+                $this->removeNode($property);
+            }
+        }
+
+        if ($defaultName === null) {
+            return null;
+        }
+
+        $defaultDescription = null;
+        $property = $node->getProperty('defaultDescription');
+        if ($property instanceof Property) {
+            $defaultDescription = $this->getValueFromProperty($property);
+
+            if ($defaultDescription !== null) {
+                $this->removeNode($property);
+            }
+        }
+
+        $node->attrGroups[] = $this->createAttributeGroup($defaultName, $defaultDescription);
+
+        return $node;
+    }
+
+    private function createAttributeGroup(string $defaultName, ?string $defaultDescription): AttributeGroup
+    {
+        $attributeGroup = $this->phpAttributeGroupFactory->createFromClass(self::ATTRIBUTE);
+
+        $attributeGroup->attrs[0]->args[] = new Arg(new String_($defaultName));
+
+        if ($defaultDescription !== null) {
+            $attributeGroup->attrs[0]->args[] = new Arg(new String_($defaultDescription));
+        }
+
+        return $attributeGroup;
+    }
+
+    private function getValueFromProperty(Property $property): ?string
+    {
+        if (count($property->props) !== 1) {
+            return null;
+        }
+        $propertyProperty = $property->props[0];
+        if ($propertyProperty->default instanceof String_) {
+            return $propertyProperty->default->value;
+        }
+
+        return null;
+    }
+}
