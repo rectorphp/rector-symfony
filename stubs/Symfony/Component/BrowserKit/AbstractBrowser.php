df561edade3487e5527a30c44099e3865821686e
Tomas Votruba <tomas.vot@gmail.com>
2022-05-06 20:31:13 +0300

    Skip Symfony\Component\HttpKernel\Client object check if Symfony\Component\BrowserKit\AbstractBrowser is missing (#164)

---
+++ stubs/Symfony/Component/BrowserKit/AbstractBrowser.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\BrowserKit;
+
+if (class_exists('Symfony\Component\BrowserKit\AbstractBrowser')) {
+    return;
+}
+
+abstract class AbstractBrowser
+{
+}
