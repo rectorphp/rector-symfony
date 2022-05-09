642ada356ce00cc85cda1fcb30ef28081c2209e6
Stephan Vierkant <stephan@vierkant.net>
2022-05-09 11:38:35 +0200

    Add CommandPropertyToAttributeRector (#163)

---
+++ tests/Rector/Class_/CommandPropertyToAttributeRector/config/configured_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Symfony\Rector\Class_\CommandPropertyToAttributeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+    $rectorConfig->rule(CommandPropertyToAttributeRector::class);
+};
