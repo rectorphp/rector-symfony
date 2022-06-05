d0c178dfa3c2b8804859e397d56ee6279f9f6038
Tomas Votruba <tomas.vot@gmail.com>
2022-06-05 11:28:42 +0200

    add fixture with requiring ctor dependency

---
+++ tests/Rector/MethodCall/FormTypeInstanceToClassConstRector/Source/AgentRequiringFormType.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Tests\Rector\MethodCall\FormTypeInstanceToClassConstRector\Source;
+
+use Symfony\Component\Form\AbstractType;
+
+final class AgentRequiringFormType extends AbstractType
+{
+}
