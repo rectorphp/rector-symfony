6d58443ced14efcd216edf5644f053b9b13bc267
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-14 13:02:15 +0700

    rollback stubs dir

---
+++ stubs/Symfony/Component/Form/FormBuilderInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\Form;
+
+if (interface_exists('Symfony\Component\Form\FormBuilderInterface')) {
+    return;
+}
+
+interface FormBuilderInterface
+{
+}
