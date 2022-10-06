77027dc4a62c2493813316191d736c92df9711ff
johan <johan@adivare.nl>
2022-10-06 14:57:59 +0200

    [CommandDescriptionToPropertyRector] Correct location for description

--- src/Rector/Class_/CommandDescriptionToPropertyRector.php
+++ src/Rector/Class_/CommandDescriptionToPropertyRector.php
@@ -165,7 +165,7 @@ CODE_SAMPLE
                 }
             }
         } else {
-            $class->stmts = array_merge($class->stmts, [$defaultDescriptionProperty]);
+            $class->stmts = array_merge([$defaultDescriptionProperty], $class->stmts);
         }
 
         return $class;
