3192dc8e398fc33feb4d6b8366cb5d434e8444d1
Johan Vlaar <JohJohan@users.noreply.github.com>
2022-11-21 17:08:59 +0100

    [SYMFONY54] replace deprecated function and classes (#282)
    Co-authored-by: johan <johan@adivare.nl>

--- config/sets/symfony/symfony54.php
+++ config/sets/symfony/symfony54.php
@@ -33,6 +33,12 @@ return static function (RectorConfig $rectorConfig): void {
             'getListeners',
             'getAuthenticators'
         ),
+        // @see https://github.com/symfony/symfony/pull/41754
+        new MethodCallRename(
+            'Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension',
+            'addSecurityListenerFactory',
+            'addAuthenticatorFactory'
+        ),
     ]);
 
     $rectorConfig->ruleWithConfiguration(RenameClassConstFetchRector::class, [
@@ -64,5 +70,13 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
         // @see https://github.com/symfony/symfony/pull/42050
         'Symfony\Component\Security\Http\Event\DeauthenticatedEvent' => 'Symfony\Component\Security\Http\Event\TokenDeauthenticatedEvent',
+        // @see https://github.com/symfony/symfony/pull/42965
+        'Symfony\Component\Cache\Adapter\DoctrineAdapter' => 'Doctrine\Common\Cache\Psr6\CacheAdapter',
+        // @see https://github.com/symfony/symfony/pull/45615
+        'Symfony\Component\HttpKernel\EventListener\AbstractTestSessionListener' => 'Symfony\Component\HttpKernel\EventListener\AbstractSessionListener',
+        'Symfony\Component\HttpKernel\EventListener\TestSessionListener' => 'Symfony\Component\HttpKernel\EventListener\SessionListener',
+        // @see https://github.com/symfony/symfony/pull/44271
+        'Symfony\Component\Notifier\Bridge\Nexmo\NexmoTransportFactory' => 'Symfony\Component\Notifier\Bridge\Vonage\VonageTransportFactory',
+        'Symfony\Component\Notifier\Bridge\Nexmo\NexmoTransport' => 'Symfony\Component\Notifier\Bridge\Vonage\VonageTransport',
     ]);
 };
