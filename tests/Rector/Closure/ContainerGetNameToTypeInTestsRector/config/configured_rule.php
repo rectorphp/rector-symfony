8e5eafa8346acf6886a4c6fea1a03d681d0a0f06
Tomas Votruba <tomas.vot@gmail.com>
2023-01-22 18:39:40 +0000

    Add ContainerGetNameToTypeInTestsRector (#334)
    * Add ContainerGetNameToTypeInTestsRector
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ tests/Rector/Closure/ContainerGetNameToTypeInTestsRector/config/configured_rule.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+use Rector\Symfony\Rector\Closure\ContainerGetNameToTypeInTestsRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+
+    $rectorConfig->rule(ContainerGetNameToTypeInTestsRector::class);
+
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/../xml/services.xml');
+};
