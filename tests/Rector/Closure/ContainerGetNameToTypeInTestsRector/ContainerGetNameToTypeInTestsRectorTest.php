9ca0f993e20278531e0e2ef7987b87b3bef40387
Tomas Votruba <tomas.vot@gmail.com>
2023-01-22 13:34:21 +0000

    Add ContainerGetNameToTypeInTestsRector

---
+++ tests/Rector/Closure/ContainerGetNameToTypeInTestsRector/ContainerGetNameToTypeInTestsRectorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Tests\Rector\Closure\ContainerGetNameToTypeInTestsRector;
+
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class ContainerGetNameToTypeInTestsRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    /**
+     * @return \Iterator<string>
+     */
+    public function provideData(): \Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
