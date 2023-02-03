f11824fa8a36915f1f41e2e42ec4c81673efaa0c
Tomas Votruba <tomas.vot@gmail.com>
2023-02-03 18:21:17 +0000

    bump to PHPUnit 10

--- tests/Rector/Closure/ContainerGetNameToTypeInTestsRector/ContainerGetNameToTypeInTestsRectorTest.php
+++ tests/Rector/Closure/ContainerGetNameToTypeInTestsRector/ContainerGetNameToTypeInTestsRectorTest.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace Rector\Symfony\Tests\Rector\Closure\ContainerGetNameToTypeInTestsRector;
 
 use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
 final class ContainerGetNameToTypeInTestsRectorTest extends AbstractRectorTestCase
 {
-    /**
-     * @dataProvider provideData()
-     */
+    #[DataProvider('provideData')]
     public function test(string $filePath): void
     {
         $this->doTestFile($filePath);
@@ -20,9 +19,9 @@ final class ContainerGetNameToTypeInTestsRectorTest extends AbstractRectorTestCa
     /**
      * @return Iterator<string>
      */
-    public function provideData(): Iterator
+    public static function provideData(): Iterator
     {
-        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
 
     public function provideConfigFilePath(): string
