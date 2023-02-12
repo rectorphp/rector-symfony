b7e7371219ea1e277bcff5c080092fa98b6cdb17
Tomas Votruba <tomas.vot@gmail.com>
2023-02-12 19:40:09 +0000

    Add ServiceArgsToServiceNamedArgRector

---
+++ tests/Rector/Closure/ServiceArgsToServiceNamedArgRector/Source/SomeClassWithConstructor.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\Symfony\Tests\Rector\Closure\ServiceArgsToServiceNamedArgRector\Source;
+
+class SomeClassWithConstructor
+{
+    public function __construct($someCtorParameter, $anotherCtorParameter)
+    {
+    }
+}
