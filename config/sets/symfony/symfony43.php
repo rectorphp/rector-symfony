09d9cae7692315f12f0749a9859481c161775d7f
Tomas Votruba <tomas.vot@gmail.com>
2023-04-06 08:27:13 +0100

    [Symfony 4.3] Add TwigBundleFilesystemLoaderToTwigRector (#384)

--- config/sets/symfony/symfony43.php
+++ config/sets/symfony/symfony43.php
@@ -13,19 +13,22 @@ use Rector\Renaming\ValueObject\MethodCallRename;
 use Rector\Symfony\Rector\MethodCall\MakeDispatchFirstArgumentEventRector;
 use Rector\Symfony\Rector\MethodCall\WebTestCaseAssertIsSuccessfulRector;
 use Rector\Symfony\Rector\MethodCall\WebTestCaseAssertResponseCodeRector;
+use Rector\Symfony\Rector\StmtsAwareInterface\TwigBundleFilesystemLoaderToTwigRector;
 
 # https://github.com/symfony/symfony/blob/4.4/UPGRADE-4.3.md
 return static function (RectorConfig $rectorConfig): void {
     # https://symfony.com/blog/new-in-symfony-4-3-better-test-assertions
-    $rectorConfig->rule(WebTestCaseAssertIsSuccessfulRector::class);
-    $rectorConfig->rule(WebTestCaseAssertResponseCodeRector::class);
+    $rectorConfig->rules([
+        WebTestCaseAssertIsSuccessfulRector::class,
+        WebTestCaseAssertResponseCodeRector::class,
+        TwigBundleFilesystemLoaderToTwigRector::class,
+        MakeDispatchFirstArgumentEventRector::class,
+    ]);
 
     $rectorConfig->ruleWithConfiguration(RenameMethodRector::class, [
         new MethodCallRename('Symfony\Component\BrowserKit\Response', 'getStatus', 'getStatusCode'),
         new MethodCallRename('Symfony\Component\Security\Http\Firewall', 'handleRequest', 'callListeners'),
     ]);
-
-    $rectorConfig->rule(MakeDispatchFirstArgumentEventRector::class);
 
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
         # https://symfony.com/blog/new-in-symfony-4-3-simpler-event-dispatching
---
+++ stubs/Symfony/Bundle/TwigBundle/Loader/FilesystemLoader.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Symfony\Bundle\TwigBundle\Loader;
+
+class FilesystemLoader
+{
+}
