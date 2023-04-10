e0791b98e1d30f48b4d1b75aa2622f5aac894988
Anton K <wiistriker@gmail.com>
2023-04-10 15:26:58 +0300

    fix ContainerServiceProvider::provideByName calls

--- src/Bridge/Symfony/ContainerServiceProvider.php
+++ src/Bridge/Symfony/ContainerServiceProvider.php
@@ -10,21 +10,32 @@ use Webmozart\Assert\Assert;
 
 final class ContainerServiceProvider
 {
+    protected ?object $container = null;
+
     public function __construct(
         private readonly RectorConfigProvider $rectorConfigProvider
     ) {
     }
 
+    protected function getSymfonyContainer(): object
+    {
+        if ($this->container === null) {
+            $symfonyContainerPhp = $this->rectorConfigProvider->getSymfonyContainerPhp();
+            Assert::fileExists($symfonyContainerPhp);
+
+            $container = require_once $symfonyContainerPhp;
+
+            // this allows older Symfony versions, e.g. 2.8 did not have the PSR yet
+            Assert::isInstanceOf($container, 'Symfony\Component\DependencyInjection\Container');
+            $this->container = $container;
+        }
+
+        return $this->container;
+    }
+
     public function provideByName(string $serviceName): object
     {
-        $symfonyContainerPhp = $this->rectorConfigProvider->getSymfonyContainerPhp();
-        Assert::fileExists($symfonyContainerPhp);
-
-        $container = require_once $symfonyContainerPhp;
-
-        // this allows older Symfony versions, e.g. 2.8 did not have the PSR yet
-        Assert::isInstanceOf($container, 'Symfony\Component\DependencyInjection\Container');
-
+        $container = $this->getSymfonyContainer();
         if (! $container->has($serviceName)) {
             $errorMessage = sprintf('Symfony container has no service "%s", maybe it is private', $serviceName);
             throw new ShouldNotHappenException($errorMessage);
