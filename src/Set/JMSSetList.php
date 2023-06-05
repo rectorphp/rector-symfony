6e60dc08133fa020678ddffe9619e7b151740594
Tomas Votruba <tomas.vot@gmail.com>
2023-06-05 14:31:59 +0200

    Remove JMSInjectPropertyToConstructorInjectionRector as niche and only of JMS rules (#425)

--- src/Set/JMSSetList.php
+++ src/Set/JMSSetList.php
@@ -14,10 +14,5 @@ final class JMSSetList implements SetListInterface
     /**
      * @var string
      */
-    final public const REMOVE_JMS_INJECT = __DIR__ . '/../../config/sets/jms/remove-jms-inject.php';
-
-    /**
-     * @var string
-     */
     final public const ANNOTATIONS_TO_ATTRIBUTES = __DIR__ . '/../../config/sets/jms/annotations-to-attributes.php';
 }
--- tests/Rector/Property/JMSInjectPropertyToConstructorInjectionRector/Source/SomeInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rector\Symfony\Tests\Rector\Property\JMSInjectPropertyToConstructorInjectionRector\Source;
-
-interface SomeInterface
-{
-}
