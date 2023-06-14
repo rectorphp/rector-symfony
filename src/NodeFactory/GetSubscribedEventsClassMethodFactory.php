872e4779416df738c3c955938dd79b366ba2eb74
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-14 11:29:57 +0700

    Update PhpDocTypeChanger call arguments usage per use of DocblockUpdater (#431)

--- src/NodeFactory/GetSubscribedEventsClassMethodFactory.php
+++ src/NodeFactory/GetSubscribedEventsClassMethodFactory.php
@@ -140,7 +140,7 @@ final class GetSubscribedEventsClassMethodFactory
 
         $returnType = new ArrayType(new StringType(), new MixedType(true));
         $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($classMethod);
-        $this->phpDocTypeChanger->changeReturnType($phpDocInfo, $returnType);
+        $this->phpDocTypeChanger->changeReturnType($classMethod, $phpDocInfo, $returnType);
     }
 
     /**
