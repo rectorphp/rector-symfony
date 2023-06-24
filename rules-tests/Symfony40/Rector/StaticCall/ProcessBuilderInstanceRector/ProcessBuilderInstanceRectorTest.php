55e05d3d8d3b351617132a395b00f7359729b7f9
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 10:58:03 +0100

    [DX] Move Symfony 4.0 rules to own category (#450)

---
+++ rules-tests/Symfony40/Rector/StaticCall/ProcessBuilderInstanceRector/ProcessBuilderInstanceRectorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Tests\Symfony40\Rector\StaticCall\ProcessBuilderInstanceRector;
+
+use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class ProcessBuilderInstanceRectorTest extends AbstractRectorTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
