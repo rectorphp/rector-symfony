54c684fe397c6120f460d650b3c2aa7d1f02912c
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 11:59:03 +0200

    [DX] Move Symfony 4.2 rules to own category

---
+++ rules-tests/Symfony42/Rector/MethodCall/ContainerGetToConstructorInjectionRector/Source/SpecificTwigExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Tests\Symfony42\Rector\MethodCall\ContainerGetToConstructorInjectionRector\Source;
+
+use Twig_ExtensionInterface;
+
+final class SpecificTwigExtension implements Twig_ExtensionInterface
+{
+}
--- rules-tests/Symfony42/Rector/MethodCall/ContainerGetToConstructorInjectionRector/config/configured_rule.php
+++ rules-tests/Symfony42/Rector/MethodCall/ContainerGetToConstructorInjectionRector/config/configured_rule.php
@@ -3,10 +3,11 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Rector\MethodCall\GetToConstructorInjectionRector;
 use Rector\Symfony\Symfony42\Rector\MethodCall\ContainerGetToConstructorInjectionRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
+
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/../xml/services.xml');
     $rectorConfig->rule(ContainerGetToConstructorInjectionRector::class);
-    $rectorConfig->rule(GetToConstructorInjectionRector::class);
 };
