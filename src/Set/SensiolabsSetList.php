4c08e657e7e0834ffd964d34cc922793cc1c40ab
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 17:11:28 +0200

    keep Set list only

--- src/Set/SensiolabsSetList.php
+++ src/Set/SensiolabsSetList.php
@@ -14,15 +14,5 @@ final class SensiolabsSetList implements SetListInterface
     /**
      * @var string
      */
-    final public const FRAMEWORK_EXTRA_50 = __DIR__ . '/../../config/sets/sensiolabs/framework-extra-50.php';
-
-    /**
-     * @var string
-     */
-    final public const FRAMEWORK_EXTRA_61 = __DIR__ . '/../../config/sets/sensiolabs/framework-extra-61.php';
-
-    /**
-     * @var string
-     */
     final public const ANNOTATIONS_TO_ATTRIBUTES = __DIR__ . '/../../config/sets/sensiolabs/annotations-to-attributes.php';
 }
