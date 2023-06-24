7b6d49f019d68178e5f813dbc89f84c67a0a3f4c
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 11:20:24 +0200

    [DX] Move Symfony 2.5 rules to own category

--- rules-tests/Symfony25/Rector/MethodCall/AddViolationToBuildViolationRector/config/configured_rule.php
+++ rules-tests/Symfony25/Rector/MethodCall/AddViolationToBuildViolationRector/config/configured_rule.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Rector\MethodCall\AddViolationToBuildViolationRector;
+use Rector\Symfony\Symfony25\Rector\MethodCall\AddViolationToBuildViolationRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
 
     $rectorConfig->rule(AddViolationToBuildViolationRector::class);
 };
