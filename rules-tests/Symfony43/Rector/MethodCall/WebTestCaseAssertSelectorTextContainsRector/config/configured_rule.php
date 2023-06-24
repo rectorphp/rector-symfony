bc34d84f3757cdff750feec6d5f5808a100a00e7
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 16:26:32 +0200

    Move WebTestCaseAssertSelectorTextContainsRector to Symfony 4.3

--- rules-tests/Symfony43/Rector/MethodCall/WebTestCaseAssertSelectorTextContainsRector/config/configured_rule.php
+++ rules-tests/Symfony43/Rector/MethodCall/WebTestCaseAssertSelectorTextContainsRector/config/configured_rule.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Rector\MethodCall\WebTestCaseAssertSelectorTextContainsRector;
+use Rector\Symfony\Symfony43\Rector\MethodCall\WebTestCaseAssertSelectorTextContainsRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
 
     $rectorConfig->rule(WebTestCaseAssertSelectorTextContainsRector::class);
 };
