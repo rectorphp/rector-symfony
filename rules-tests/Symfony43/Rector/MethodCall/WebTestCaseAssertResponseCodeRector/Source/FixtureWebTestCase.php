0ae6edbee43594165bb4379fdf0dd047f21909fa
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 12:04:34 +0200

    [DX] Move Symfony 4.3 rules to own category

---
+++ rules-tests/Symfony43/Rector/MethodCall/WebTestCaseAssertResponseCodeRector/Source/FixtureWebTestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Tests\Symfony43\Rector\MethodCall\WebTestCaseAssertResponseCodeRector\Source;
+
+class FixtureWebTestCase
+{
+
+}
--- tests/Rector/ClassMethod/TemplateAnnotationToThisRenderRector/Source/ClassToBeCreated.php
+++ tests/Rector/ClassMethod/TemplateAnnotationToThisRenderRector/Source/ClassToBeCreated.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rector\Symfony\Tests\Rector\ClassMethod\TemplateAnnotationToThisRenderRector\Source;
+namespace Rector\Symfony\Tests\Symfony43\Rector\ClassMethod\TemplateAnnotationToThisRenderRector\Source;
 
 final class ClassToBeCreated
 {
