90ca2b85fe97c4c617564917d1c948c142980e68
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 16:01:08 +0100

    Move ContainerGetNameToTypeInTestsRector to Symfony 3.4 category (#471)

--- rules-tests/Symfony34/Rector/Closure/ContainerGetNameToTypeInTestsRector/config/configured_rule.php
+++ rules-tests/Symfony34/Rector/Closure/ContainerGetNameToTypeInTestsRector/config/configured_rule.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
-use Rector\Symfony\Rector\Closure\ContainerGetNameToTypeInTestsRector;
+use Rector\Symfony\Symfony34\Rector\Closure\ContainerGetNameToTypeInTestsRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
 
     $rectorConfig->rule(ContainerGetNameToTypeInTestsRector::class);
 
