3d36207c395f64a5d947501300249ceca6d9b6f2
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 11:11:33 +0100

    [DX] Move Symfony 4.4 rules to own category (#453)

---
+++ rules-tests/Symfony44/Rector/MethodCall/AuthorizationCheckerIsGrantedExtractorRector/Source/DifferentClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rector\Symfony\Tests\Symfony44\Rector\MethodCall\AuthorizationCheckerIsGrantedExtractorRector\Source;
+
+class DifferentClass
+{
+    public function isGranted($args)
+    {
+    }
+
+    public function differentMethod($args)
+    {
+    }
+}
