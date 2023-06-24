fb2827781ef8ca9bad51a330b8846be942bf4b31
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 11:45:33 +0200

    [DX] Move Symfony 3.4 rules to own category

--- config/sets/symfony/symfony34.php
+++ config/sets/symfony/symfony34.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Removing\Rector\ClassMethod\ArgumentRemoverRector;
 use Rector\Removing\ValueObject\ArgumentRemover;
-use Rector\Symfony\Rector\ClassMethod\MergeMethodAnnotationToRouteAnnotationRector;
+use Rector\Symfony\Symfony34\Rector\ClassMethod\MergeMethodAnnotationToRouteAnnotationRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(ArgumentRemoverRector::class, [
