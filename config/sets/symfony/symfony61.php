8aefe47521bdaa4d432c8c868627d16477da645f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-06-24 22:39:06 +0700

    Register RemoveServiceFromSensioRouteRector and MagicClosureTwigExtensionToNativeMethodsRector to config set (#474)

--- config/sets/symfony/symfony61.php
+++ config/sets/symfony/symfony61.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Symfony\Symfony61\Rector\Class_\CommandPropertyToAttributeRector;
+use Rector\Symfony\Symfony61\Rector\Class_\MagicClosureTwigExtensionToNativeMethodsRector;
 use Rector\Symfony\Symfony61\Rector\StaticPropertyFetch\ErrorNamesPropertyToConstantRector;
 
 # https://github.com/symfony/symfony/blob/6.1/UPGRADE-6.1.md
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([CommandPropertyToAttributeRector::class, ErrorNamesPropertyToConstantRector::class]);
+    $rectorConfig->rules([
+        CommandPropertyToAttributeRector::class,
+        ErrorNamesPropertyToConstantRector::class,
+        MagicClosureTwigExtensionToNativeMethodsRector::class,
+    ]);
 
     $rectorConfig->ruleWithConfiguration(
         RenameClassRector::class,
