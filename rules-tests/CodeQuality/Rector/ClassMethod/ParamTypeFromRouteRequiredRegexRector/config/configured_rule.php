6492ccb7a811c065066f22fb917ac1078f6e9206
Tomas Votruba <tomas.vot@gmail.com>
2023-06-24 15:38:18 +0200

    Move ParamTypeFromRouteRequiredRegexRector to code quality category

--- rules-tests/CodeQuality/Rector/ClassMethod/ParamTypeFromRouteRequiredRegexRector/config/configured_rule.php
+++ rules-tests/CodeQuality/Rector/ClassMethod/ParamTypeFromRouteRequiredRegexRector/config/configured_rule.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Rector\ClassMethod\ParamTypeFromRouteRequiredRegexRector;
+use Rector\Symfony\CodeQuality\Rector\ClassMethod\ParamTypeFromRouteRequiredRegexRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
