bc87324a683014f030e00d14e61712b8c4358172
Tomas Votruba <tomas.vot@gmail.com>
2023-06-26 08:47:23 +0100

    Move GetToConstructorInjectionRector to Symfony 2.8 set (#478)

--- rules-tests/Symfony28/Rector/MethodCall/GetToConstructorInjectionRector/config/configured_rule.php
+++ rules-tests/Symfony28/Rector/MethodCall/GetToConstructorInjectionRector/config/configured_rule.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Rector\MethodCall\GetToConstructorInjectionRector;
+use Rector\Symfony\Symfony28\Rector\MethodCall\GetToConstructorInjectionRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
