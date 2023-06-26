ea012e18f647743f7be16d773c86409a8adae02c
Tomas Votruba <tomas.vot@gmail.com>
2023-06-26 09:49:18 +0200

    add GetToConstructorInjectionRector to set

--- config/sets/symfony/symfony28.php
+++ config/sets/symfony/symfony28.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 use Rector\Arguments\Rector\ClassMethod\ReplaceArgumentDefaultValueRector;
 use Rector\Arguments\ValueObject\ReplaceArgumentDefaultValue;
 use Rector\Config\RectorConfig;
+use Rector\Symfony\Symfony28\Rector\MethodCall\GetToConstructorInjectionRector;
 use Rector\Symfony\Symfony28\Rector\StaticCall\ParseFileRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(ParseFileRector::class);
+    $rectorConfig->rules([
+        ParseFileRector::class,
+        GetToConstructorInjectionRector::class,
+    ]);
 
     $rectorConfig->ruleWithConfiguration(ReplaceArgumentDefaultValueRector::class, [
         // @see https://github.com/symfony/symfony/commit/912fc4de8fd6de1e5397be4a94d39091423e5188
