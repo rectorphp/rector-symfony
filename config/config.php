791c0dc0a664be88b2c2b90d80a2f81bb894685c
Tomas Votruba <tomas.vot@gmail.com>
2023-08-10 21:00:06 +0200

    remove rename non-php file rule

--- config/config.php
+++ config/config.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\NonPhpFile\Rector\RenameClassNonPhpRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $services = $rectorConfig->services();
@@ -15,6 +14,4 @@ return static function (RectorConfig $rectorConfig): void {
 
     $services->load('Rector\\Symfony\\', __DIR__ . '/../src')
         ->exclude([__DIR__ . '/../src/Rector', __DIR__ . '/../src/ValueObject']);
-
-    $rectorConfig->rule(RenameClassNonPhpRector::class);
 };
