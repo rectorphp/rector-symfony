32eaf4467674f590e3e21e0c2accfdf7f010e6d1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-09-07 20:03:07 +0700

    [Configs] Alias SymfonyRoutesProviderInterface into SymfonyRoutesProvider (#518)

--- rules-tests/Configs/Rector/ClassMethod/AddRouteAnnotationRector/config/configured_rule.php
+++ rules-tests/Configs/Rector/ClassMethod/AddRouteAnnotationRector/config/configured_rule.php
@@ -3,11 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Contract\Bridge\Symfony\Routing\SymfonyRoutesProviderInterface;
+
+use Rector\Symfony\Bridge\Symfony\Routing\SymfonyRoutesProvider;
 use Rector\Symfony\Configs\Rector\ClassMethod\AddRouteAnnotationRector;
+use Rector\Symfony\Contract\Bridge\Symfony\Routing\SymfonyRoutesProviderInterface;
 use Rector\Symfony\Tests\Configs\Rector\ClassMethod\AddRouteAnnotationRector\Source\DummySymfonyRoutesProvider;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->singleton(SymfonyRoutesProvider::class);
+    $rectorConfig->alias(SymfonyRoutesProvider::class, SymfonyRoutesProviderInterface::class);
+
     $rectorConfig->rule(AddRouteAnnotationRector::class);
 
     // only for tests
