7c731e4ff251235fd2c5b229248865306ccc1eee
Tomas Votruba <tomas.vot@gmail.com>
2023-09-30 21:50:11 +0200

    add support for command parent call

--- stubs/Symfony/Bundle/FrameworkBundle/Command/ContainerAwareCommand.php
+++ stubs/Symfony/Bundle/FrameworkBundle/Command/ContainerAwareCommand.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Symfony\Bundle\FrameworkBundle\Command;
 
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-abstract class ContainerAwareCommand
+abstract class ContainerAwareCommand extends Command
 {
     private string $something;
 
