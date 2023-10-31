748b11b9599ce645a1be13281a20a169756e5a2b
Jille Behm <alleknalle@users.noreply.github.com>
2023-10-31 15:42:42 +0100

    Class as string

--- config/sets/symfony/symfony50-types.php
+++ config/sets/symfony/symfony50-types.php
@@ -295,14 +295,14 @@ return static function (RectorConfig $rectorConfig): void {
             'Symfony\Component\Form\DataMapperInterface',
             'mapFormsToData',
             0,
-            new ObjectType(Traversable::class)
+            new ObjectType('Traversable')
         ),
         // @see https://github.com/symfony/symfony/commit/ce77be2507631cd12e4ca37510dab37f4c2b759a
         new AddParamTypeDeclaration(
             'Symfony\Component\Form\DataMapperInterface',
             'mapDataToForms',
             1,
-            new ObjectType(Traversable::class)
+            new ObjectType('Traversable')
         ),
         new AddParamTypeDeclaration('Symfony\Component\Form\Form', 'add', 1, new StringType()),
         new AddParamTypeDeclaration('Symfony\Component\Form\Form', 'remove', 0, new StringType()),
