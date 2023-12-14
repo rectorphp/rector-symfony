b7df4d80df2f7707b5bbf9fd4d0d881a7250adde
Stefan Talen <s.r.talen@gmail.com>
2023-12-14 22:24:29 +0100

    Separating Swiftmailer to Symfony Mailer from Symfony 5.3 set (#548)

---
+++ config/sets/swiftmailer/swiftmailer-to-symfony-mailer.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
+use Rector\Symfony\Symfony53\Rector\MethodCall\SwiftCreateMessageToNewEmailRector;
+use Rector\Symfony\Symfony53\Rector\MethodCall\SwiftSetBodyToHtmlPlainMethodCallRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    // @see https://symfony.com/blog/the-end-of-swiftmailer
+    $rectorConfig->rules([
+        SwiftCreateMessageToNewEmailRector::class,
+        SwiftSetBodyToHtmlPlainMethodCallRector::class,
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        'Swift_Mailer' => 'Symfony\Component\Mailer\MailerInterface',
+        'Swift_Message' => 'Symfony\Component\Mime\Email',
+        // message
+        'Swift_Mime_SimpleMessage' => 'Symfony\Component\Mime\RawMessage',
+        // transport
+        'Swift_SmtpTransport' => 'Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport',
+        'Swift_FailoverTransport' => 'Symfony\Component\Mailer\Transport\FailoverTransport',
+        'Swift_SendmailTransport' => 'Symfony\Component\Mailer\Transport\SendmailTransport',
+    ]);
+};
