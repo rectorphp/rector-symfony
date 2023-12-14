2276b7ee9be182dce0fcd4ad21b48ed098ca63b8
Stefan Talen <s.r.talen@gmail.com>
2023-12-14 21:32:40 +0100

    Adding rename rules

--- config/sets/symfony/symfony64.php
+++ config/sets/symfony/symfony64.php
@@ -3,7 +3,15 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
 // @see https://github.com/symfony/symfony/blob/6.4/UPGRADE-6.4.md
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->ruleWithConfiguration(
+        RenameClassRector::class,
+        [
+            'Symfony\Component\HttpKernel\UriSigner' => 'Symfony\Component\HttpFoundation\UriSigner',
+            'Symfony\Component\HttpKernel\Debug\FileLinkFormatter' => 'Symfony\Component\ErrorHandler\ErrorRenderer\FileLinkFormatter',
+        ],
+    );
 };
