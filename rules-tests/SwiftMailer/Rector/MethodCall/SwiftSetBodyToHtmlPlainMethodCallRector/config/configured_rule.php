3294a99c263e5b67f4f275ca2db29435af9d615d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-12-15 13:09:12 +0700

    rename tests

--- rules-tests/SwiftMailer/Rector/MethodCall/SwiftSetBodyToHtmlPlainMethodCallRector/config/configured_rule.php
+++ rules-tests/SwiftMailer/Rector/MethodCall/SwiftSetBodyToHtmlPlainMethodCallRector/config/configured_rule.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Symfony53\Rector\MethodCall\SwiftSetBodyToHtmlPlainMethodCallRector;
+use Rector\Symfony\SwiftMailer\Rector\MethodCall\SwiftSetBodyToHtmlPlainMethodCallRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(SwiftSetBodyToHtmlPlainMethodCallRector::class);
