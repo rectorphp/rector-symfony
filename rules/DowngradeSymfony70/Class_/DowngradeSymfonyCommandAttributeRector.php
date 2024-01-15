525a850ac12ac7944db984d24d64218121da312b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-15 15:12:33 +0700

    [DowngradeSymfony70] Add DowngradeSymfonyCommandAttributeRector

---
+++ rules/DowngradeSymfony70/Class_/DowngradeSymfonyCommandAttributeRector.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\DowngradeSymfony70\Rector\MethodCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class DowngradeSymfonyCommandAttributeRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Changes createMessage() into a new Symfony\Component\Mime\Email',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+#[AsCommand(name: 'app:create-user', description: 'some description')]
+class CreateUserCommand extends Command
+{}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+class CreateUserCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('app:create-user');
+        $this->setDescription('some description');
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Class_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return null;
+    }
+}
