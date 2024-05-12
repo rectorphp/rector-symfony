9ffc14310e5ba227be5eeddb4d791824d5d7cb80
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-05-12 14:27:11 +0700

    [CodeQuality] Add KernelBrowser support on LiteralGetToRequestClassConstantRector

--- stubs/Symfony/Bundle/FrameworkBundle/Test/WebTestCase.php
+++ stubs/Symfony/Bundle/FrameworkBundle/Test/WebTestCase.php
@@ -9,15 +9,23 @@ if (class_exists('Symfony\Bundle\FrameworkBundle\Test\WebTestCase')) {
 }
 
 use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 
 abstract class WebTestCase extends KernelTestCase
 {
     /**
-     * @var Client
+     * @var Client|KernelBrowser
      */
     protected static $client;
 
     public static function getClient(): Client
+    {
+        return self::$client;
+    }
+
+    // support both Client and KernelBrowser transformation
+    // @see https://github.com/rectorphp/rector/issues/8613
+    public static function createClient(array $options = [], array $server = []): KernelBrowser
     {
         return self::$client;
     }
