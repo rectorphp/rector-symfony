4fb36db7efbf71ed2e5dd9aaa47f888dfebfb0dc
Stefan Talen <s.r.talen@gmail.com>
2024-05-27 15:56:39 +0200

    Convert \Swift_Message to an Symfony\Component\Mime\Email (#603)

--- config/sets/swiftmailer/swiftmailer-to-symfony-mailer.php
+++ config/sets/swiftmailer/swiftmailer-to-symfony-mailer.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\ClassConstFetch\RenameClassConstFetchRector;
 use Rector\Renaming\Rector\Name\RenameClassRector;
+use Rector\Renaming\ValueObject\RenameClassAndConstFetch;
+use Rector\Symfony\SwiftMailer\Rector\ClassMethod\Swift_MessageToEmailRector;
 use Rector\Symfony\SwiftMailer\Rector\MethodCall\SwiftCreateMessageToNewEmailRector;
 use Rector\Symfony\SwiftMailer\Rector\MethodCall\SwiftSetBodyToHtmlPlainMethodCallRector;
 
@@ -12,16 +15,45 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
         SwiftCreateMessageToNewEmailRector::class,
         SwiftSetBodyToHtmlPlainMethodCallRector::class,
+        Swift_MessageToEmailRector::class,
     ]);
 
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
         'Swift_Mailer' => 'Symfony\Component\Mailer\MailerInterface',
-        'Swift_Message' => 'Symfony\Component\Mime\Email',
         // message
         'Swift_Mime_SimpleMessage' => 'Symfony\Component\Mime\RawMessage',
         // transport
         'Swift_SmtpTransport' => 'Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport',
         'Swift_FailoverTransport' => 'Symfony\Component\Mailer\Transport\FailoverTransport',
         'Swift_SendmailTransport' => 'Symfony\Component\Mailer\Transport\SendmailTransport',
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(RenameClassConstFetchRector::class, [
+        new RenameClassAndConstFetch(
+            'Swift_Mime_SimpleMessage',
+            'PRIORITY_HIGHEST',
+            'Symfony\Component\Mime\Email',
+            'PRIORITY_HIGHEST'
+        ), new RenameClassAndConstFetch(
+            'Swift_Mime_SimpleMessage',
+            'PRIORITY_HIGH',
+            'Symfony\Component\Mime\Email',
+            'PRIORITY_HIGH'
+        ), new RenameClassAndConstFetch(
+            'Swift_Mime_SimpleMessage',
+            'PRIORITY_NORMAL',
+            'Symfony\Component\Mime\Email',
+            'PRIORITY_NORMAL'
+        ), new RenameClassAndConstFetch(
+            'Swift_Mime_SimpleMessage',
+            'PRIORITY_LOW',
+            'Symfony\Component\Mime\Email',
+            'PRIORITY_LOW'
+        ), new RenameClassAndConstFetch(
+            'Swift_Mime_SimpleMessage',
+            'PRIORITY_LOWEST',
+            'Symfony\Component\Mime\Email',
+            'PRIORITY_LOWEST'
+        ),
     ]);
 };
