0160fc9babb0e6957adc38b714a562e6c7f58b2b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-05-27 22:08:35 +0700

    more example

--- stubs/SwiftMailer/Swift_Message.php
+++ stubs/SwiftMailer/Swift_Message.php
@@ -6,4 +6,64 @@ if (class_exists('Swift_Message')) {
 
 class Swift_Message
 {
+    public function setSubject($subject)
+    {
+        return $this;
+    }
+
+    public function setPriority($priority)
+    {
+        return $this;
+    }
+
+    public function addBcc($address, $name = null)
+    {
+        return $this;
+    }
+
+    public function addCc($address, $name = null)
+    {
+        return $this;
+    }
+
+    public function addFrom($address, $name = null)
+    {
+        return $this;
+    }
+
+    public function addReplyTo($address, $name = null)
+    {
+        return $this;
+    }
+
+    public function addTo($address, $name = null)
+    {
+        return $this;
+    }
+
+    public function setBcc($addresses, $name = null)
+    {
+        return $this;
+    }
+
+    public function setCc($addresses, $name = null)
+    {
+        return $this;
+    }
+
+
+    public function setFrom($addresses, $name = null)
+    {
+        return $this;
+    }
+
+    public function setReplyTo($addresses, $name = null)
+    {
+        return $this;
+    }
+
+    public function setTo($addresses, $name = null)
+    {
+        return $this;
+    }
 }
