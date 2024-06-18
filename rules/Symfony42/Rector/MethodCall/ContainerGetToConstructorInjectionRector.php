50c45ae49714fc6cfad2d897f27dde317cda3200
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-18 20:49:54 +0700

    Check first class callable early on ContainerGetToConstructorInjectionRector

--- rules/Symfony42/Rector/MethodCall/ContainerGetToConstructorInjectionRector.php
+++ rules/Symfony42/Rector/MethodCall/ContainerGetToConstructorInjectionRector.php
@@ -110,8 +110,12 @@ CODE_SAMPLE
                 return null;
             }
 
+            if ($node->isFirstClassCallable()) {
+                return null;
+            }
+
             $args = $node->getArgs();
-            if (is_array($args) && count($args) === 1 && $args[0]->value->getType() === 'Expr_ClassConstFetch') {
+            if (count($args) === 1 && $args[0]->value->getType() === 'Expr_ClassConstFetch') {
                 return null;
             }
 
