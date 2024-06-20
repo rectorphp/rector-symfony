b4dfb48b3f76277d11885d7fcfb9874b5006b988
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-20 14:58:56 +0700

    [Twig] Use RectorConfigBuilder on twig sets (#613)
    * [Twig] Use RectorConfigBuilder on twig sets
    
    * [Twig] Use RectorConfigBuilder on twig sets
    
    * mirror comment

--- config/sets/twig/twig140.php
+++ config/sets/twig/twig140.php
@@ -4,6 +4,5 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/twig-underscore-to-namespace.php');
-};
+return RectorConfig::configure()
+    ->withSets([__DIR__ . '/twig-underscore-to-namespace.php']);
