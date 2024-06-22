e5392caa31ef3b7a36e578b55d393368713369b3
Tomas Votruba <tomas.vot@gmail.com>
2024-06-22 22:14:17 +0900

    [Symfony 6.1] Add CommandConfigureToAttributeRector

---
+++ config/sets/symfony/symfony70.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+// @see https://github.com/symfony/symfony/blob/7.0/UPGRADE-7.0.md
+return static function (RectorConfig $rectorConfig): void {
+};
---
+++ rules/Symfony61/Rector/Class_/CommandConfigureToAttributeRector.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Symfony61\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Attribute;
+use PhpParser\Node\AttributeGroup;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Property;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\ObjectType;
+use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
+use Rector\PhpAttribute\NodeFactory\PhpAttributeGroupFactory;
+use Rector\Rector\AbstractRector;
+use Rector\Symfony\Enum\SymfonyAnnotation;
+use Rector\Symfony\NodeAnalyzer\Command\AttributeValueResolver;
+use Rector\Symfony\NodeAnalyzer\Command\SetAliasesMethodCallExtractor;
+use Rector\ValueObject\PhpVersionFeature;
+use Rector\VersionBonding\Contract\MinPhpVersionInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://symfony.com/doc/current/console.html#registering-the-command
+ *
+ * @see \Rector\Symfony\Tests\Symfony61\Rector\Class_\CommandConfigureToAttributeRector\CommandConfigureToAttributeRectorTest
+ */
+final class CommandConfigureToAttributeRector extends AbstractRector implements MinPhpVersionInterface
+{
+    public function __construct(
+        private readonly PhpAttributeGroupFactory $phpAttributeGroupFactory,
+        private readonly PhpAttributeAnalyzer $phpAttributeAnalyzer,
+        private readonly AttributeValueResolver $attributeValueResolver,
+        private readonly ReflectionProvider $reflectionProvider,
+        private readonly SetAliasesMethodCallExtractor $setAliasesMethodCallExtractor,
+    ) {
+    }
+
+    public function provideMinPhpVersion(): int
+    {
+        return PhpVersionFeature::ATTRIBUTES;
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Add Symfony\Component\Console\Attribute\AsCommand to Symfony Commands from configure()',
+            [
+                new CodeSample(<<<'CODE_SAMPLE'
+use Symfony\Component\Console\Command\Command;
+
+final class SunshineCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('sunshine');
+    }
+}
+CODE_SAMPLE
+                    , <<<'CODE_SAMPLE'
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+
+#[AsCommand('sunshine')]
+final class SunshineCommand extends Command
+{
+}
+CODE_SAMPLE),
+
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Class_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isObjectType($node, new ObjectType('Symfony\\Component\\Console\\Command\\Command'))) {
+            return null;
+        }
+
+        if (! $this->reflectionProvider->hasClass(SymfonyAnnotation::AS_COMMAND)) {
+            return null;
+        }
+
+        // @todo move configure() to attribute
+    }
+}
