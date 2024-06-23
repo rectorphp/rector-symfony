10b011df707ee0d625f8a9b74e5c872d84bd37c1
Tomas Votruba <tomas.vot@gmail.com>
2024-06-23 11:26:32 +0900

    [Symfony 7.0 and 7.1] Extend sets based on upgrade notes (#627)
    * add object normalizer class type preference
    
    * request matcher classes
    
    * add request matcher changes, add requires annotation upgrade
    
    * improve Symfony 7.0 and 7.1 sets

--- config/sets/symfony/symfony71.php
+++ config/sets/symfony/symfony71.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\Class_\RenameAttributeRector;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
 use Rector\Renaming\ValueObject\RenameAttribute;
 
 // @see https://github.com/symfony/symfony/blob/7.1/UPGRADE-7.1.md
-return static function (RectorConfig $rectorConfig): void {
+
+return RectorConfig::configure()
     // @see https://github.com/symfony/symfony/blob/7.1/UPGRADE-7.1.md#dependencyinjection
-    $rectorConfig->ruleWithConfiguration(RenameAttributeRector::class, [
+    ->withConfiguredRule(RenameAttributeRector::class, [
         new RenameAttribute(
             'Symfony\Component\DependencyInjection\Attribute\TaggedIterator',
             'Symfony\Component\DependencyInjection\Attribute\AutowireIterator'
@@ -18,5 +21,12 @@ return static function (RectorConfig $rectorConfig): void {
             'Symfony\Component\DependencyInjection\Attribute\TaggedLocator',
             'Symfony\Component\DependencyInjection\Attribute\AutowireLocator'
         ),
+    ])
+    ->withConfiguredRule(RenameMethodRector::class, [
+        // typo fix
+        new MethodCallRename(
+            '̈́Symfony\Component\Serializer\Context\Normalizer\AbstractNormalizerContextBuilder',
+            'withDefaultContructorArguments',
+            'withDefaultConstructorArguments',
+        ),
     ]);
-};
