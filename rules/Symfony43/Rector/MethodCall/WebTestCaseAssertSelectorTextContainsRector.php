d8a2c6297ff1563542e33ddba23a5e870576af71
Tomas Votruba <tomas.vot@gmail.com>
2024-06-26 16:22:03 +0200

    one more

--- rules/Symfony43/Rector/MethodCall/WebTestCaseAssertSelectorTextContainsRector.php
+++ rules/Symfony43/Rector/MethodCall/WebTestCaseAssertSelectorTextContainsRector.php
@@ -7,6 +7,7 @@ namespace Rector\Symfony\Symfony43\Rector\MethodCall;
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
+use Rector\Configuration\Deprecation\Contract\DeprecatedInterface;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -14,7 +15,7 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 /**
  * @deprecated This rule is deprecated since Rector 1.1.2, as it does not upgrade to valid code.
  */
-final class WebTestCaseAssertSelectorTextContainsRector extends AbstractRector
+final class WebTestCaseAssertSelectorTextContainsRector extends AbstractRector implements DeprecatedInterface
 {
     private bool $hasWarned = false;
 
