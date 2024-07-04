0c3ea77acd2799db357db2604e67542ac872239b
Tomas Votruba <tomas.vot@gmail.com>
2024-07-04 13:23:31 +0200

    add param name with _

--- rules-tests/Configs/Rector/Class_/AutowireAttributeRector/Source/configs/app_config.php
+++ rules-tests/Configs/Rector/Class_/AutowireAttributeRector/Source/configs/app_config.php
@@ -1,5 +1,6 @@
 <?php
 
+use Rector\Symfony\Tests\Configs\Rector\Class_\AutowireAttributeRector\Fixture\AnotherConfiguredService;
 use Rector\Symfony\Tests\Configs\Rector\Class_\AutowireAttributeRector\Fixture\SkipServiceWithoutConstructor;
 use Rector\Symfony\Tests\Configs\Rector\Class_\AutowireAttributeRector\Fixture\SomeConfiguredService;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -10,6 +11,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(SomeConfiguredService::class)
         ->arg('timeout', '%timeout%')
         ->arg('key', '%env(APP_KEY)%');
+
+    $services->set(AnotherConfiguredService::class)
+        ->arg('missing', '%MISSING_PARAM%')
+        ->arg('someParameter', '%SOME_PARAM%');
 
     // should be skipped
     $services->set(SkipServiceWithoutConstructor::class)
