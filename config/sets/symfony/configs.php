57342ac428d8f7c44002d5b443971dfa30076f9b
Tomas Votruba <tomas.vot@gmail.com>
2024-07-27 12:46:53 +0200

    Deprecate ServicesSetNameToSetTypeRector as it creates invalid code

--- config/sets/symfony/configs.php
+++ config/sets/symfony/configs.php
@@ -6,7 +6,6 @@ use Rector\Config\RectorConfig;
 use Rector\Symfony\Configs\Rector\Closure\ServiceArgsToServiceNamedArgRector;
 use Rector\Symfony\Configs\Rector\Closure\ServiceSetStringNameToClassNameRector;
 use Rector\Symfony\Configs\Rector\Closure\ServiceSettersToSettersAutodiscoveryRector;
-use Rector\Symfony\Configs\Rector\Closure\ServicesSetNameToSetTypeRector;
 use Rector\Symfony\Configs\Rector\Closure\ServiceTagsToDefaultsAutoconfigureRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -14,7 +13,6 @@ return static function (RectorConfig $rectorConfig): void {
         ServiceArgsToServiceNamedArgRector::class,
         ServiceSetStringNameToClassNameRector::class,
         ServiceSettersToSettersAutodiscoveryRector::class,
-        ServicesSetNameToSetTypeRector::class,
         ServiceTagsToDefaultsAutoconfigureRector::class,
     ]);
 };
