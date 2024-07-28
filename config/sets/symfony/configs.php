053b225683e4a099d72385d80503fc0b3ed118ae
Tomas Votruba <tomas.vot@gmail.com>
2024-07-28 12:11:01 +0200

    [Configs] Add MergeServiceNameTypeRector (#650)

--- config/sets/symfony/configs.php
+++ config/sets/symfony/configs.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Symfony\Configs\Rector\Closure\MergeServiceNameTypeRector;
 use Rector\Symfony\Configs\Rector\Closure\ServiceArgsToServiceNamedArgRector;
 use Rector\Symfony\Configs\Rector\Closure\ServiceSetStringNameToClassNameRector;
 use Rector\Symfony\Configs\Rector\Closure\ServiceSettersToSettersAutodiscoveryRector;
@@ -10,6 +11,7 @@ use Rector\Symfony\Configs\Rector\Closure\ServiceTagsToDefaultsAutoconfigureRect
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
+        MergeServiceNameTypeRector::class,
         ServiceArgsToServiceNamedArgRector::class,
         ServiceSetStringNameToClassNameRector::class,
         ServiceSettersToSettersAutodiscoveryRector::class,
