fb220f0b8c8dec517ce196347160685311a1a306
Tomas Votruba <tomas.vot@gmail.com>
2024-07-30 13:29:23 +0200

    [Config] Add ParameterBagToAutowireAttributeRector

--- rules/Configs/Rector/Class_/AutowireAttributeRector.php
+++ rules/Configs/Rector/Class_/AutowireAttributeRector.php
@@ -30,6 +30,8 @@ use Webmozart\Assert\Assert;
 /**
  * The param/env is only available since Symfony 6.3
  * @see https://symfony.com/blog/new-in-symfony-6-3-dependency-injection-improvements#new-options-for-autowire-attribute
+ *
+ * @see \Rector\Symfony\Tests\Configs\Rector\Class_\AutowireAttributeRector\AutowireAttributeRectorTest
  */
 final class AutowireAttributeRector extends AbstractRector implements ConfigurableRectorInterface
 {
