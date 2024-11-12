5b930b00a065255598e41b4d1111acfc4112f871
Tomas Votruba <tomas.vot@gmail.com>
2024-11-12 22:15:03 +0100

    remove deprecated set list marker

--- src/Set/SwiftMailerSetList.php
+++ src/Set/SwiftMailerSetList.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Rector\Symfony\Set;
 
-use Rector\Set\Contract\SetListInterface;
-
 /**
  * @api
  */
-final class SwiftMailerSetList implements SetListInterface
+final class SwiftMailerSetList
 {
     /**
      * @var string
