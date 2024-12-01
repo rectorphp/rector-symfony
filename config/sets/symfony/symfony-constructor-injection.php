1744d4af62c98b834ece263dbeaf0771d4110c96
Tomas Votruba <tomas.vot@gmail.com>
2024-12-01 17:04:11 +0100

    register rule

--- config/sets/symfony/symfony-constructor-injection.php
+++ config/sets/symfony/symfony-constructor-injection.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Symfony\DependencyInjection\Rector\Class_\CommandGetByTypeToConstructorInjectionRector;
 use Rector\Symfony\DependencyInjection\Rector\Class_\ControllerGetByTypeToConstructorInjectionRector;
+use Rector\Symfony\DependencyInjection\Rector\Trait_\TraitGetByTypeToInjectRector;
 use Rector\Symfony\Symfony28\Rector\MethodCall\GetToConstructorInjectionRector;
 use Rector\Symfony\Symfony34\Rector\Closure\ContainerGetNameToTypeInTestsRector;
 use Rector\Symfony\Symfony42\Rector\MethodCall\ContainerGetToConstructorInjectionRector;
@@ -14,6 +15,7 @@ return static function (RectorConfig $rectorConfig): void {
         // modern step-by-step narrow approach
         ControllerGetByTypeToConstructorInjectionRector::class,
         CommandGetByTypeToConstructorInjectionRector::class,
+        TraitGetByTypeToInjectRector::class,
 
         // legacy rules that require container fetch
         ContainerGetToConstructorInjectionRector::class,
