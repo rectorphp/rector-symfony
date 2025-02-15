e986666c623126a1267a236d3e7f0a621f99520c
Tomas Votruba <tomas.vot@gmail.com>
2025-02-15 22:05:01 +0100

    [code-quality] Add attribute support

--- src/Enum/SymfonyAttribute.php
+++ src/Enum/SymfonyAttribute.php
@@ -15,4 +15,9 @@ final class SymfonyAttribute
      * @var string
      */
     public const EVENT_LISTENER_ATTRIBUTE = 'Symfony\Component\EventDispatcher\Attribute\AsEventListener';
+
+    /**
+     * @var string
+     */
+    public const ROUTE = 'Symfony\Component\Routing\Attribute\Route';
 }
