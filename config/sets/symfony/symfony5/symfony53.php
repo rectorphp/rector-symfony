5b4e9d44cde09c8182ec2591e9234a817271ec7a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-05 06:50:03 +0700

    add symfony53-mailer

--- config/sets/symfony/symfony5/symfony53.php
+++ config/sets/symfony/symfony5/symfony53.php
@@ -3,13 +3,10 @@
 declare(strict_types=1);
 
 use PHPStan\Type\ObjectType;
-use PHPStan\Type\StringType;
 use Rector\Config\RectorConfig;
 use Rector\Symfony\Symfony53\Rector\StaticPropertyFetch\KernelTestCaseContainerPropertyDeprecationRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
 use Rector\TypeDeclaration\ValueObject\AddParamTypeDeclaration;
-use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
 
 # https://github.com/symfony/symfony/blob/5.4/UPGRADE-5.3.md
 
@@ -20,14 +17,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/symfony53/symfony53-console.php');
     $rectorConfig->import(__DIR__ . '/symfony53/symfony53-http-kernel.php');
     $rectorConfig->import(__DIR__ . '/symfony53/symfony53-security-core.php');
-
-    $rectorConfig->ruleWithConfiguration(AddReturnTypeDeclarationRector::class, [
-        new AddReturnTypeDeclaration(
-            'Symfony\Component\Mailer\Transport\AbstractTransportFactory',
-            'getEndpoint',
-            new StringType(),
-        ),
-    ]);
+    $rectorConfig->import(__DIR__ . '/symfony53/symfony53-mailer.php');
 
     $rectorConfig->ruleWithConfiguration(AddParamTypeDeclarationRector::class, [
         // @see https://github.com/symfony/symfony/commit/ce77be2507631cd12e4ca37510dab37f4c2b759a
--- src/Set/SetProvider/Symfony5SetProvider.php
+++ src/Set/SetProvider/Symfony5SetProvider.php
@@ -223,6 +223,13 @@ final class Symfony5SetProvider implements SetProviderInterface
 
             new ComposerTriggeredSet(
                 SetGroup::SYMFONY,
+                'symfony/security-mailer',
+                '5.3',
+                __DIR__ . '/../../../config/sets/symfony/symfony5/symfony53/symfony53-mailer.php'
+            ),
+
+            new ComposerTriggeredSet(
+                SetGroup::SYMFONY,
                 'symfony/*',
                 '5.3',
                 __DIR__ . '/../../../config/sets/symfony/symfony5/symfony53.php'
