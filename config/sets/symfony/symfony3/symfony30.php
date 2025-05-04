03006a3a7c7d8ab80e8698c6aad257ff6b4b3e0e
Tomas Votruba <tomas.vot@gmail.com>
2025-05-04 15:44:02 +0200

    [config] split of translation and bridge monolog

--- config/sets/symfony/symfony3/symfony30.php
+++ config/sets/symfony/symfony3/symfony30.php
@@ -3,9 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Renaming\Rector\Name\RenameClassRector;
-use Rector\Renaming\ValueObject\MethodCallRename;
 
 /**
  * @resource https://github.com/symfony/symfony/blob/3.4/UPGRADE-3.0.md
@@ -20,23 +18,10 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/symfony30/symfony30-http-foundation.php');
     $rectorConfig->import(__DIR__ . '/symfony30/symfony30-http-kernel.php');
     $rectorConfig->import(__DIR__ . '/symfony30/symfony30-validator.php');
-
-    $rectorConfig->ruleWithConfiguration(RenameMethodRector::class, [
-        // monolog
-        new MethodCallRename('Symfony\Bridge\Monolog\Logger', 'emerg', 'emergency'),
-        new MethodCallRename('Symfony\Bridge\Monolog\Logger', 'crit', 'critical'),
-        new MethodCallRename('Symfony\Bridge\Monolog\Logger', 'err', 'error'),
-        new MethodCallRename('Symfony\Bridge\Monolog\Logger', 'warn', 'warning'),
-
-        // translator
-        new MethodCallRename('Symfony\Component\Translation\Dumper\FileDumper', 'format', 'formatCatalogue'),
-        new MethodCallRename('Symfony\Component\Translation\Translator', 'getMessages', 'getCatalogue'),
-    ]);
+    $rectorConfig->import(__DIR__ . '/symfony30/symfony30-translation.php');
+    $rectorConfig->import(__DIR__ . '/symfony30/symfony30-bridge-monolog.php');
 
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
-        // partial with method rename
-        'Symfony\Bridge\Monolog\Logger' => 'Psr\Log\LoggerInterface',
-
         // twig
         'Symfony\Bundle\TwigBundle\TwigDefaultEscapingStrategy' => 'Twig_FileExtensionEscapingStrategy',
 
