4dd339aea1d52e19bb67bcea8950d1b7bd8ffe5b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-05 06:55:27 +0700

    add symfony53

--- src/Set/SetProvider/Symfony5SetProvider.php
+++ src/Set/SetProvider/Symfony5SetProvider.php
@@ -195,6 +195,13 @@ final class Symfony5SetProvider implements SetProviderInterface
 
             new ComposerTriggeredSet(
                 SetGroup::SYMFONY,
+                'symfony/symfony',
+                '5.3',
+                __DIR__ . '/../../../config/sets/symfony/symfony5/symfony53.php'
+            ),
+
+            new ComposerTriggeredSet(
+                SetGroup::SYMFONY,
                 'symfony/http-foundation',
                 '5.3',
                 __DIR__ . '/../../../config/sets/symfony/symfony5/symfony53/symfony53-http-foundation.php'
