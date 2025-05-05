4c55ea5ba441779698c13cfac34c11b19f196693
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-05 11:53:21 +0700

    add security

--- config/sets/symfony/symfony5/symfony54.php
+++ config/sets/symfony/symfony5/symfony54.php
@@ -21,8 +21,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/symfony54/symfony54-security.php');
 
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
-        // @see https://github.com/symfony/symfony/pull/42050
-        'Symfony\Component\Security\Http\Event\DeauthenticatedEvent' => 'Symfony\Component\Security\Http\Event\TokenDeauthenticatedEvent',
         // @see https://github.com/symfony/symfony/pull/42965
         'Symfony\Component\Cache\Adapter\DoctrineAdapter' => 'Doctrine\Common\Cache\Psr6\CacheAdapter',
         // @see https://github.com/symfony/symfony/pull/45615
