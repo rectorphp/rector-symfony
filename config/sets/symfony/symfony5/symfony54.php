7bef68c54441bc5f8e97c7e74e555aaa86e3bce9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-05 12:01:36 +0700

    add notifier

--- config/sets/symfony/symfony5/symfony54.php
+++ config/sets/symfony/symfony5/symfony54.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Renaming\Rector\Name\RenameClassRector;
 
 # https://github.com/symfony/symfony/blob/5.x/UPGRADE-5.4.md
 
@@ -15,10 +14,5 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/symfony54/symfony54-security.php');
     $rectorConfig->import(__DIR__ . '/symfony54/symfony54-cache.php');
     $rectorConfig->import(__DIR__ . '/symfony54/symfony54-http-kernel.php');
-
-    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
-        // @see https://github.com/symfony/symfony/pull/44271
-        'Symfony\Component\Notifier\Bridge\Nexmo\NexmoTransportFactory' => 'Symfony\Component\Notifier\Bridge\Vonage\VonageTransportFactory',
-        'Symfony\Component\Notifier\Bridge\Nexmo\NexmoTransport' => 'Symfony\Component\Notifier\Bridge\Vonage\VonageTransport',
-    ]);
+    $rectorConfig->import(__DIR__ . '/symfony54/symfony54-notifier.php');
 };
--- src/Set/SetProvider/Symfony5SetProvider.php
+++ src/Set/SetProvider/Symfony5SetProvider.php
@@ -293,6 +293,13 @@ final class Symfony5SetProvider implements SetProviderInterface
 
             new ComposerTriggeredSet(
                 SetGroup::SYMFONY,
+                'symfony/notifier',
+                '5.4',
+                __DIR__ . '/../../../config/sets/symfony/symfony5/symfony54/symfony54-notifier.php'
+            ),
+
+            new ComposerTriggeredSet(
+                SetGroup::SYMFONY,
                 'symfony/*',
                 '5.4',
                 __DIR__ . '/../../../config/sets/symfony/symfony5/symfony54.php'
