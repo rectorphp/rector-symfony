bba3285d9ffebe305c1b87941017fa45713bbef3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-06 23:13:57 +0700

    add console

--- config/sets/symfony/symfony6/symfony61.php
+++ config/sets/symfony/symfony6/symfony61.php
@@ -3,19 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Symfony61\Rector\Class_\CommandConfigureToAttributeRector;
-use Rector\Symfony\Symfony61\Rector\Class_\CommandPropertyToAttributeRector;
 use Rector\Symfony\Symfony61\Rector\Class_\MagicClosureTwigExtensionToNativeMethodsRector;
 
 # https://github.com/symfony/symfony/blob/6.1/UPGRADE-6.1.md
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
-        CommandConfigureToAttributeRector::class,
-        CommandPropertyToAttributeRector::class,
         MagicClosureTwigExtensionToNativeMethodsRector::class,
     ]);
 
     $rectorConfig->import(__DIR__ . '/symfony61/symfony61-serializer.php');
     $rectorConfig->import(__DIR__ . '/symfony61/symfony61-validator.php');
+    $rectorConfig->import(__DIR__ . '/symfony61/symfony61-console.php');
 };
---
+++ config/sets/symfony/symfony6/symfony61/symfony61-console.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Symfony\Symfony61\Rector\Class_\CommandConfigureToAttributeRector;
+use Rector\Symfony\Symfony61\Rector\Class_\CommandPropertyToAttributeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        CommandConfigureToAttributeRector::class,
+        CommandPropertyToAttributeRector::class,
+    ]);
+};
