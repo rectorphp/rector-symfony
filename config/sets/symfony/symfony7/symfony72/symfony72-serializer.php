8f51df285fa7326e3518ebf1645fa699dbec8ddf
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-07 10:56:51 +0700

    [symfony 72] split of configs per package

--- config/sets/symfony/symfony7/symfony72/symfony72-serializer.php
+++ config/sets/symfony/symfony7/symfony72/symfony72-serializer.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\Name\RenameClassRector;
 
-// @see https://github.com/symfony/symfony/blame/7.2/UPGRADE-7.2.md
-
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
         // @see https://github.com/symfony/symfony/blob/7.2/UPGRADE-7.2.md#serializer
