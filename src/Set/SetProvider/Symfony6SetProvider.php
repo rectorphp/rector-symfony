f6349746231991b943f4cf50068c89ed28ce9d27
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-07 21:23:14 +0700

    add routing

--- src/Set/SetProvider/Symfony6SetProvider.php
+++ src/Set/SetProvider/Symfony6SetProvider.php
@@ -126,6 +126,12 @@ final class Symfony6SetProvider implements SetProviderInterface
                 '6.0',
                 __DIR__ . '/../../../config/sets/symfony/symfony6/symfony60/symfony60-property-info.php'
             ),
+            new ComposerTriggeredSet(
+                SetGroup::SYMFONY,
+                'symfony/routing',
+                '6.0',
+                __DIR__ . '/../../../config/sets/symfony/symfony6/symfony60/symfony60-routing.php'
+            ),
 
             new ComposerTriggeredSet(
                 SetGroup::SYMFONY,
