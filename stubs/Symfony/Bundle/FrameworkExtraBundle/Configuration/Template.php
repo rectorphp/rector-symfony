fcf920ef9f29bd1232a69aa26a881518ba5cf591
Tomas Votruba <tomas.vot@gmail.com>
2025-05-07 17:46:54 +0200

    cs

--- stubs/Symfony/Bundle/FrameworkExtraBundle/Configuration/Template.php
+++ stubs/Symfony/Bundle/FrameworkExtraBundle/Configuration/Template.php
@@ -2,7 +2,7 @@
 
 namespace Sensio\Bundle\FrameworkExtraBundle\Configuration;
 
-#[\Attribute]
+#[\Attribute(\Attribute::TARGET_METHOD)]
 class Template
 {
     public function __construct(string $templatePath)
