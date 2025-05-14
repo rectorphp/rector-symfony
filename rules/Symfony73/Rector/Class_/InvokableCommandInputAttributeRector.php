13a39904cda576b8142c4f8188244ab401717a41
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-05-14 17:19:04 +0700

    Fix ClassConstFetch use on InvokableCommandInputAttributeRector (#778)
    * Fix ClassConstFetch use on InvokableCommandInputAttributeRector
    
    * Fix ClassConstFetch use on InvokableCommandInputAttributeRector
    
    * Fix ClassConstFetch use on InvokableCommandInputAttributeRector

--- rules/Symfony73/Rector/Class_/InvokableCommandInputAttributeRector.php
+++ rules/Symfony73/Rector/Class_/InvokableCommandInputAttributeRector.php
@@ -18,6 +18,7 @@ use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Expression;
 use Rector\Doctrine\NodeAnalyzer\AttributeFinder;
 use Rector\Exception\ShouldNotHappenException;
+use Rector\PhpParser\Node\Value\ValueResolver;
 use Rector\Rector\AbstractRector;
 use Rector\Symfony\Enum\CommandMethodName;
 use Rector\Symfony\Enum\SymfonyAttribute;
@@ -40,7 +41,8 @@ final class InvokableCommandInputAttributeRector extends AbstractRector
     public function __construct(
         private readonly AttributeFinder $attributeFinder,
         private readonly CommandArgumentsAndOptionsResolver $commandArgumentsAndOptionsResolver,
-        private readonly CommandInvokeParamsFactory $commandInvokeParamsFactory
+        private readonly CommandInvokeParamsFactory $commandInvokeParamsFactory,
+        private readonly ValueResolver $valueResolver
     ) {
     }
 
@@ -237,7 +239,7 @@ CODE_SAMPLE
 
     private function replaceInputArgumentOptionFetchWithVariables(ClassMethod $executeClassMethod): void
     {
-        $this->traverseNodesWithCallable($executeClassMethod->stmts, function (Node $node): null|Variable|ClassConstFetch|ConstFetch {
+        $this->traverseNodesWithCallable($executeClassMethod->stmts, function (Node $node): ?Variable {
             if (! $node instanceof MethodCall) {
                 return null;
             }
@@ -254,7 +256,7 @@ CODE_SAMPLE
                 ->value;
 
             if ($firstArgValue instanceof ClassConstFetch || $firstArgValue instanceof ConstFetch) {
-                return $firstArgValue;
+                return new Variable($this->valueResolver->getValue($firstArgValue));
             }
 
             if (! $firstArgValue instanceof String_) {
