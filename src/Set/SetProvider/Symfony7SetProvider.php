0b3b4fd4b0538d3a49943019d5c571502d76563f
Matthias Schmidt <matthias@mttsch.dev>
2025-07-10 15:51:01 +0200

    [Symfony 74] Rename Application::add() to Application::addCommand() (#813)

--- src/Set/SetProvider/Symfony7SetProvider.php
+++ src/Set/SetProvider/Symfony7SetProvider.php
@@ -123,6 +123,12 @@ final class Symfony7SetProvider implements SetProviderInterface
                 '7.4',
                 __DIR__ . '/../../../config/sets/symfony/symfony7/symfony74/symfony74-console.php'
             ),
+            new ComposerTriggeredSet(
+                SetGroup::SYMFONY,
+                'symfony/framework-bundle',
+                '7.4',
+                __DIR__ . '/../../../config/sets/symfony/symfony7/symfony74/symfony74-framework-bundle.php'
+            ),
         ];
     }
 }
