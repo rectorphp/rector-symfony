5880ed93d50a8b5460bc51727a6edd6d1a9deb64
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-09-10 23:35:44 +0700

    [Symfony73] Remove extends AbstractExtension after apply change on GetFunctionsToAsTwigFunctionAttributeRector (#846)
    * Update Fixtures for GetFunctionsToAsTwigFunctionAttributeRector
    
    * Closes #823 Fixes https://github.com/rectorphp/rector/issues/9361
    
    * skip whole
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Christophe VERGNE <christophe.vergne@gmail.com>

--- rules/Symfony73/GetMethodToAsTwigAttributeTransformer.php
+++ rules/Symfony73/GetMethodToAsTwigAttributeTransformer.php
@@ -19,6 +19,7 @@ use PhpParser\Node\Stmt\Return_;
 use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Type\ObjectType;
 use Rector\Privatization\NodeManipulator\VisibilityManipulator;
+use Rector\Symfony\Enum\TwigClass;
 use Rector\Symfony\Symfony73\NodeAnalyzer\LocalArrayMethodCallableMatcher;
 use Rector\Symfony\Symfony73\NodeRemover\ReturnEmptyArrayMethodRemover;
 
@@ -52,8 +53,9 @@ final readonly class GetMethodToAsTwigAttributeTransformer
             return false;
         }
 
-        $hasChanged = false;
+        $originalMethod = clone $getMethod;
 
+        $hasChanged = false;
         foreach ((array) $getMethod->stmts as $stmt) {
             // handle return array simple case
             if (! $stmt instanceof Return_) {
@@ -89,7 +91,8 @@ final readonly class GetMethodToAsTwigAttributeTransformer
                 if ($this->isLocalCallable($secondArg->value)) {
                     $localMethodName = $this->localArrayMethodCallableMatcher->match($secondArg->value, $objectType);
                     if (! is_string($localMethodName)) {
-                        continue;
+                        $getMethod = $originalMethod;
+                        return false;
                     }
 
                     $localMethod = $class->getMethod($localMethodName);
@@ -108,6 +111,10 @@ final readonly class GetMethodToAsTwigAttributeTransformer
             }
 
             $this->returnEmptyArrayMethodRemover->removeClassMethodIfArrayEmpty($class, $returnArray, $methodName);
+        }
+
+        if ($hasChanged && $class->extends instanceof FullyQualified && $class->extends->toString() === TwigClass::TWIG_EXTENSION) {
+            $class->extends = null;
         }
 
         return $hasChanged;
