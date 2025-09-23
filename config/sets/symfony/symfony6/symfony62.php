293607e889ec5a6576e95ccbd16c3dee0fc89222
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-09-23 19:35:17 +0700

    [Symfony 6.2] Move symfony/security-core to security-http and security-bundle as target class/contant exists there

--- config/sets/symfony/symfony6/symfony62.php
+++ config/sets/symfony/symfony6/symfony62.php
@@ -19,7 +19,7 @@ return static function (RectorConfig $rectorConfig): void {
         new AnnotationToAttribute('Sensio\Bundle\FrameworkExtraBundle\Configuration\Template'),
     ]);
 
-    $rectorConfig->import(__DIR__ . '/symfony62/symfony62-security-core.php');
+    $rectorConfig->import(__DIR__ . '/symfony62/symfony62-security-bundle.php');
     $rectorConfig->import(__DIR__ . '/symfony62/symfony62-security-http.php');
     $rectorConfig->import(__DIR__ . '/symfony62/symfony62-mime.php');
     $rectorConfig->import(__DIR__ . '/symfony62/symfony62-http-kernel.php');
