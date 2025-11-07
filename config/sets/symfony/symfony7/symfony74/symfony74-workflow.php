6ac13d687c1dcb358ef6d5e35610676d165b9773
MrYamous <matt.lempereur@gmail.com>
2025-11-07 09:47:52 +0100

    [Symfony74] update WorkflowDumpCommand namespace

---
+++ config/sets/symfony/symfony7/symfony74/symfony74-workflow.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    // @see https://github.com/symfony/symfony/blob/7.4/UPGRADE-7.4.md#frameworkbundle
+    $rectorConfig->ruleWithConfiguration(
+        RenameClassRector::class,
+        [
+            'Symfony\Bundle\FrameworkBundle\Command\WorkflowDumpCommand' => 'Symfony\Component\Workflow\Command\WorkflowDumpCommand',
+        ],
+    );
+};
