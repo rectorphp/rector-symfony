a5ff686981ae0e9f70d8ae68792f586a593e4f7c
Tomas Votruba <tomas.vot@gmail.com>
2025-11-21 01:15:29 +0000

    tv skip param converter (#876)
    * add fixture
    
    * skip param converter param types

--- stubs/Symfony/Bundle/FrameworkExtraBundle/Configuration/ParamConverter.php
+++ stubs/Symfony/Bundle/FrameworkExtraBundle/Configuration/ParamConverter.php
@@ -4,4 +4,15 @@ namespace Sensio\Bundle\FrameworkExtraBundle\Configuration;
 
 class ParamConverter
 {
+    /**
+     * @param array|string $data
+     */
+    public function __construct(
+        $data = [],
+        string $class = null,
+        array $options = [],
+        bool $isOptional = false,
+        string $converter = null
+    ) {
+    }
 }
