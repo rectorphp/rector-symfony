40e67e0489ad0adbdd011b1653f45374e6baf622
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-11-24 20:19:26 +0700

    [CodeQuality] Do not replace variable assign on ControllerMethodInjectionToConstructorRector (#883)
    * [CodeQuality] Do not replace variable assign on ControllerMethodInjectionToConstructorRector
    
    * fix

--- rules/CodeQuality/Rector/Class_/ControllerMethodInjectionToConstructorRector.php
+++ rules/CodeQuality/Rector/Class_/ControllerMethodInjectionToConstructorRector.php
@@ -14,6 +14,7 @@ use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use Rector\Doctrine\NodeAnalyzer\AttributeFinder;
 use Rector\NodeManipulator\ClassDependencyManipulator;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\PhpParser\Node\Value\ValueResolver;
 use Rector\PostRector\ValueObject\PropertyMetadata;
 use Rector\Rector\AbstractRector;
@@ -190,7 +191,10 @@ CODE_SAMPLE
 
                 if (! $this->isNames($node, $paramNamesToReplace)) {
                     return null;
+                }
 
+                if ($node->getAttribute(AttributeKey::IS_BEING_ASSIGNED) === true) {
+                    return null;
                 }
 
                 $propertyName = $this->getName($node);
