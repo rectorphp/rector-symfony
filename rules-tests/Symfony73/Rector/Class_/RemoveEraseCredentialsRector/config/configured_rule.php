e7836995094c30240edf3c98c556e8da6becf0ab
Tomas Votruba <tomas.vot@gmail.com>
2025-11-30 20:08:54 +0100

    [symfony 7.3] Add RemoveEraseCredentialsRector

--- rules-tests/Symfony73/Rector/Class_/RemoveEraseCredentialsRector/config/configured_rule.php
+++ rules-tests/Symfony73/Rector/Class_/RemoveEraseCredentialsRector/config/configured_rule.php
@@ -3,9 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Symfony73\Rector\Class_\AddVoteArgumentToVoteOnAttributeRector;
 use Rector\Symfony\Symfony73\Rector\Class_\RemoveEraseCredentialsRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([AddVoteArgumentToVoteOnAttributeRector::class, RemoveEraseCredentialsRector::class]);
+    $rectorConfig->rule(RemoveEraseCredentialsRector::class);
 };
