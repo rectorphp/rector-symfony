000389ae0222b084d1ab7801590fdf49b9ffdec3
Tomas Votruba <tomas.vot@gmail.com>
2025-12-06 15:53:25 +0100

    fixup! Fix PushRequestToRequestStackConstructorRector when RequestStack is created without parentheses (#897)

--- rules/CodeQuality/Rector/Class_/ControllerMethodInjectionToConstructorRector.php
+++ rules/CodeQuality/Rector/Class_/ControllerMethodInjectionToConstructorRector.php
@@ -154,8 +154,11 @@ CODE_SAMPLE
                         SymfonyClass::REQUEST,
                         FosClass::PARAM_FETCHER,
                         SymfonyClass::UUID,
+<<<<<<< HEAD
                         \Throwable::class,
                         \Exception::class,
+=======
+>>>>>>> 50e7b0b7 (fixup! Fix PushRequestToRequestStackConstructorRector when RequestStack is created without parentheses (#897))
                         ...$entityClasses,
                     ]
                 )) {
