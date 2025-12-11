f78703c653a4213a1e0bfdd6b20a69a9fd346dea
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-12-11 20:09:40 +0700

    Remove InvokableCommandInputAttributeRector from set

--- config/sets/symfony/symfony7/symfony73/symfony73-console.php
+++ config/sets/symfony/symfony7/symfony73/symfony73-console.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Symfony\Symfony73\Rector\Class_\CommandDefaultNameAndDescriptionToAsCommandAttributeRector;
 use Rector\Symfony\Symfony73\Rector\Class_\CommandHelpToAttributeRector;
-use Rector\Symfony\Symfony73\Rector\Class_\InvokableCommandInputAttributeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rules([
         CommandHelpToAttributeRector::class,
-        InvokableCommandInputAttributeRector::class,
         CommandDefaultNameAndDescriptionToAsCommandAttributeRector::class,
     ]);
 };
