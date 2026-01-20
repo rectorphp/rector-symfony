ae1f8abeb10990ca7bfc9b6042fcf986768d6e5c
Mohammed DARHOUCHE <techm.76@gmail.com>
2026-01-20 09:11:02 +0100

    [Symfony 7.3] Replace AuthorizationChecker with AccessDecisionManager in voters (#913)

--- rules-tests/Symfony73/Rector/Class_/AuthorizationCheckerToAccessDecisionManagerInVoterRector/config/configured_rule.php
+++ rules-tests/Symfony73/Rector/Class_/AuthorizationCheckerToAccessDecisionManagerInVoterRector/config/configured_rule.php
@@ -3,12 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Symfony73\Rector\Class_\AddVoteArgumentToVoteOnAttributeRector;
 use Rector\Symfony\Symfony73\Rector\Class_\AuthorizationCheckerToAccessDecisionManagerInVoterRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([
-        AddVoteArgumentToVoteOnAttributeRector::class,
+return RectorConfig::configure()
+    ->withRules([
         AuthorizationCheckerToAccessDecisionManagerInVoterRector::class,
     ]);
-};
