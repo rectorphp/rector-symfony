82e3035d3649dbd34ef8eb6feaa6345059f1a916
Florian Hofsaess <florian.hofsaess@check24.de>
2026-06-04 21:08:28 +0200

    [Symfony81] Add new rule for deprecated validator test usages

---
+++ config/sets/symfony/symfony8/symfony81/symfony81-validator.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Symfony\Symfony81\Rector\MethodCall\ConstraintValidatorValidateToValidateInContextRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        ConstraintValidatorValidateToValidateInContextRector::class,
+    ]);
+};
