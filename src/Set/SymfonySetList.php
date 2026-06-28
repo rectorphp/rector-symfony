8c854a4314065cb04277e1729be8c397aaae6d6f
Tomas Votruba <tomas.vot@gmail.com>
2026-06-28 16:16:40 +0200

    Use @deprecated docblock over native attribute on set list constants (#953)
    Switch the constants to @deprecated docblocks, which IDEs and Rector
    still report but PHP does not emit at runtime. Skip
    DeprecatedAnnotationToDeprecatedAttributeRector so Rector does not
    convert them back to the attribute.
    
    Fixes rectorphp/rector#9788

--- src/Set/SymfonySetList.php
+++ src/Set/SymfonySetList.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\Symfony\Set;
 
-use Deprecated;
-
 /**
  * @api
  */
@@ -13,173 +11,177 @@ final class SymfonySetList
 {
     public const string CONFIGS = __DIR__ . '/../../config/sets/symfony/configs.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_25 = __DIR__ . '/../../config/sets/symfony/symfony25.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_26 = __DIR__ . '/../../config/sets/symfony/symfony26.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_27 = __DIR__ . '/../../config/sets/symfony/symfony27.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_28 = __DIR__ . '/../../config/sets/symfony/symfony28.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_30 = __DIR__ . '/../../config/sets/symfony/symfony3/symfony30.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_31 = __DIR__ . '/../../config/sets/symfony/symfony3/symfony31.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_32 = __DIR__ . '/../../config/sets/symfony/symfony3/symfony32.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_33 = __DIR__ . '/../../config/sets/symfony/symfony3/symfony33.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_34 = __DIR__ . '/../../config/sets/symfony/symfony3/symfony34.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_40 = __DIR__ . '/../../config/sets/symfony/symfony4/symfony40.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_41 = __DIR__ . '/../../config/sets/symfony/symfony4/symfony41.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_42 = __DIR__ . '/../../config/sets/symfony/symfony4/symfony42.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_43 = __DIR__ . '/../../config/sets/symfony/symfony4/symfony43.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_44 = __DIR__ . '/../../config/sets/symfony/symfony4/symfony44.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_50 = __DIR__ . '/../../config/sets/symfony/symfony5/symfony50.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_50_TYPES = __DIR__ . '/../../config/sets/symfony/symfony5/symfony50/symfony50-types.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_51 = __DIR__ . '/../../config/sets/symfony/symfony5/symfony51.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_52 = __DIR__ . '/../../config/sets/symfony/symfony5/symfony52.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_53 = __DIR__ . '/../../config/sets/symfony/symfony5/symfony53.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_54 = __DIR__ . '/../../config/sets/symfony/symfony5/symfony54.php';
 
-    #[Deprecated(message: 'Use ->withAttributesSets(symfony: true) in rector.php config instead')]
+    /**
+     * @deprecated Use ->withAttributesSets(symfony: true) in rector.php config instead
+     */
     public const string SYMFONY_52_VALIDATOR_ATTRIBUTES = __DIR__ . '/../../config/sets/symfony/symfony5/symfony52-validator-attributes.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_60 = __DIR__ . '/../../config/sets/symfony/symfony6/symfony60.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_61 = __DIR__ . '/../../config/sets/symfony/symfony6/symfony61.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_62 = __DIR__ . '/../../config/sets/symfony/symfony6/symfony62.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_63 = __DIR__ . '/../../config/sets/symfony/symfony6/symfony63.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_64 = __DIR__ . '/../../config/sets/symfony/symfony6/symfony64.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_70 = __DIR__ . '/../../config/sets/symfony/symfony7/symfony70.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_71 = __DIR__ . '/../../config/sets/symfony/symfony7/symfony71.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_72 = __DIR__ . '/../../config/sets/symfony/symfony7/symfony72.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_73 = __DIR__ . '/../../config/sets/symfony/symfony7/symfony73.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_74 = __DIR__ . '/../../config/sets/symfony/symfony7/symfony74.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_80 = __DIR__ . '/../../config/sets/symfony/symfony8/symfony80.php';
 
-    #[Deprecated(
-        message: 'Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead'
-    )]
+    /**
+     * @deprecated Set list are too generic and do not handle package differences. Use ->withComposerBased(symfony: true) instead
+     */
     public const string SYMFONY_81 = __DIR__ . '/../../config/sets/symfony/symfony8/symfony81.php';
 
     public const string SYMFONY_CODE_QUALITY = __DIR__ . '/../../config/sets/symfony/symfony-code-quality.php';
 
     public const string SYMFONY_CONSTRUCTOR_INJECTION = __DIR__ . '/../../config/sets/symfony/symfony-constructor-injection.php';
 
-    #[Deprecated(message: 'Use ->withAttributesSets(symfony: true) in rector.php config instead')]
+    /**
+     * @deprecated Use ->withAttributesSets(symfony: true) in rector.php config instead
+     */
     public const string ANNOTATIONS_TO_ATTRIBUTES = __DIR__ . '/../../config/sets/symfony/annotations-to-attributes.php';
 }
