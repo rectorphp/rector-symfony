beeb3b1be01ac11be5c9018766f16acfaf9b9af6
Tomas Votruba <tomas.vot@gmail.com>
2026-07-13 12:17:17 +0200

    [Deprecation] Deprecate WebTestCaseAssertIsSuccessfulRector and WebTestCaseAssertResponseCodeRector
    They hide the asserted behavior and force different assert methods for different status codes. Assert the HTTP status code directly instead.
    
    Gut both rules to throw on deprecation, remove them from the symfony43-framework-bundle set, drop their tests.

--- rules/Symfony43/Rector/MethodCall/WebTestCaseAssertIsSuccessfulRector.php
+++ rules/Symfony43/Rector/MethodCall/WebTestCaseAssertIsSuccessfulRector.php
@@ -5,32 +5,36 @@ declare(strict_types=1);
 namespace Rector\Symfony\Symfony43\Rector\MethodCall;
 
 use PhpParser\Node;
-use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
-use PhpParser\Node\Scalar\String_;
-use Rector\NodeAnalyzer\ExprAnalyzer;
-use Rector\PhpParser\Node\Value\ValueResolver;
-use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
+use Rector\Configuration\Deprecation\Contract\DeprecatedInterface;
+use Rector\Exception\ShouldNotHappenException;
 use Rector\Rector\AbstractRector;
-use Rector\Symfony\NodeAnalyzer\SymfonyTestCaseAnalyzer;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
- * @changelog https://symfony.com/blog/new-in-symfony-4-3-better-test-assertions
- * @changelog https://github.com/symfony/symfony/pull/30813
- *
- * @see \Rector\Symfony\Tests\Symfony43\Rector\MethodCall\WebTestCaseAssertIsSuccessfulRector\WebTestCaseAssertIsSuccessfulRectorTest
+ * @deprecated This rule hides the asserted behavior and forces different assert methods for different status codes. Assert the HTTP status code directly instead.
  */
-final class WebTestCaseAssertIsSuccessfulRector extends AbstractRector
+final class WebTestCaseAssertIsSuccessfulRector extends AbstractRector implements DeprecatedInterface
 {
-    public function __construct(
-        private readonly SymfonyTestCaseAnalyzer $symfonyTestCaseAnalyzer,
-        private readonly TestsNodeAnalyzer $testsNodeAnalyzer,
-        private readonly ExprAnalyzer $exprAnalyzer,
-        private readonly ValueResolver $valueResolver,
-    ) {
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class, StaticCall::class];
+    }
+
+    /**
+     * @param MethodCall|StaticCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        throw new ShouldNotHappenException(sprintf(
+            '"%s" is deprecated, as it hides the asserted behavior and forces different assert methods for different status codes. Assert the HTTP status code directly instead',
+            self::class
+        ));
     }
 
     public function getRuleDefinition(): RuleDefinition
@@ -62,57 +66,5 @@ class SomeClass extends TestCase
 CODE_SAMPLE
             ),
         ]);
-    }
-
-    /**
-     * @return array<class-string<Node>>
-     */
-    public function getNodeTypes(): array
-    {
-        return [MethodCall::class, StaticCall::class];
-    }
-
-    /**
-     * @param MethodCall|StaticCall $node
-     */
-    public function refactor(Node $node): ?Node
-    {
-        if (! $this->symfonyTestCaseAnalyzer->isInWebTestCase($node)) {
-            return null;
-        }
-
-        if (! $this->testsNodeAnalyzer->isAssertMethodCallName($node, 'assertSame')) {
-            return null;
-        }
-
-        $args = $node->getArgs();
-        if ($this->valueResolver->getValue($args[0]->value, true) !== 200) {
-            return null;
-        }
-
-        $secondArg = $args[1]->value;
-        if (! $secondArg instanceof MethodCall) {
-            return null;
-        }
-
-        if (! $this->isName($secondArg->name, 'getStatusCode')) {
-            return null;
-        }
-
-        $newArgs = [];
-        // When we had a custom message argument we want to add it to the new assert.
-        if (isset($args[2])) {
-            if ($this->exprAnalyzer->isDynamicExpr($args[2]->value)) {
-                $newArgs[] = $args[2]->value;
-            } else {
-                $newArgs[] = new Arg(new String_($this->valueResolver->getValue($args[2]->value, true)));
-            }
-        }
-
-        if ($node instanceof StaticCall) {
-            return $this->nodeFactory->createStaticCall('self', 'assertResponseIsSuccessful', $newArgs);
-        }
-
-        return $this->nodeFactory->createLocalMethodCall('assertResponseIsSuccessful', $newArgs);
     }
 }
--- rules/Symfony43/Rector/MethodCall/WebTestCaseAssertResponseCodeRector.php
+++ rules/Symfony43/Rector/MethodCall/WebTestCaseAssertResponseCodeRector.php
@@ -5,33 +5,36 @@ declare(strict_types=1);
 namespace Rector\Symfony\Symfony43\Rector\MethodCall;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
-use PHPStan\Type\ObjectType;
-use Rector\NodeAnalyzer\ExprAnalyzer;
-use Rector\PhpParser\Node\Value\ValueResolver;
-use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
+use Rector\Configuration\Deprecation\Contract\DeprecatedInterface;
+use Rector\Exception\ShouldNotHappenException;
 use Rector\Rector\AbstractRector;
-use Rector\Symfony\CodeQuality\Enum\ResponseClass;
-use Rector\Symfony\NodeAnalyzer\SymfonyTestCaseAnalyzer;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
- * @changelog https://symfony.com/blog/new-in-symfony-4-3-better-test-assertions
- * @changelog https://github.com/symfony/symfony/pull/30813
- *
- * @see \Rector\Symfony\Tests\Symfony43\Rector\MethodCall\WebTestCaseAssertResponseCodeRector\WebTestCaseAssertResponseCodeRectorTest
+ * @deprecated This rule hides the asserted behavior and forces different assert methods for different status codes. Assert the HTTP status code directly instead.
  */
-final class WebTestCaseAssertResponseCodeRector extends AbstractRector
+final class WebTestCaseAssertResponseCodeRector extends AbstractRector implements DeprecatedInterface
 {
-    public function __construct(
-        private readonly SymfonyTestCaseAnalyzer $symfonyTestCaseAnalyzer,
-        private readonly TestsNodeAnalyzer $testsNodeAnalyzer,
-        private readonly ExprAnalyzer $exprAnalyzer,
-        private readonly ValueResolver $valueResolver,
-    ) {
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class, StaticCall::class];
+    }
+
+    /**
+     * @param MethodCall|StaticCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        throw new ShouldNotHappenException(sprintf(
+            '"%s" is deprecated, as it hides the asserted behavior and forces different assert methods for different status codes. Assert the HTTP status code directly instead',
+            self::class
+        ));
     }
 
     public function getRuleDefinition(): RuleDefinition
@@ -67,131 +70,5 @@ final class SomeClass extends WebTestCase
 CODE_SAMPLE
             ),
         ]);
-    }
-
-    /**
-     * @return array<class-string<Node>>
-     */
-    public function getNodeTypes(): array
-    {
-        return [MethodCall::class, StaticCall::class];
-    }
-
-    /**
-     * @param MethodCall|StaticCall $node
-     */
-    public function refactor(Node $node): ?Node
-    {
-        if (! $this->symfonyTestCaseAnalyzer->isInWebTestCase($node)) {
-            return null;
-        }
-
-        $newMethodCall = $this->processAssertResponseStatusCodeSame($node);
-        if ($newMethodCall !== null) {
-            return $newMethodCall;
-        }
-
-        return $this->processAssertResponseRedirects($node);
-    }
-
-    /**
-     * We look for: "$client->getResponse()->headers->get('Location')"
-     */
-    public function isGetLocationMethodCall(Expr $expr): bool
-    {
-        if (! $expr instanceof MethodCall) {
-            return false;
-        }
-
-        if (! $this->isName($expr->name, 'get')) {
-            return false;
-        }
-
-        $args = $expr->getArgs();
-        if ($args === []) {
-            return false;
-        }
-
-        $firstArg = $args[0];
-        return $this->valueResolver->isValue($firstArg->value, 'Location');
-    }
-
-    private function processAssertResponseStatusCodeSame(StaticCall|MethodCall $methodCall): MethodCall|StaticCall|null
-    {
-        if (! $this->isName($methodCall->name, 'assertSame')) {
-            return null;
-        }
-
-        $args = $methodCall->getArgs();
-
-        $secondArg = $args[1];
-        if (! $secondArg->value instanceof MethodCall) {
-            return null;
-        }
-
-        $nestedMethodCall = $secondArg->value;
-
-        if (! $this->isName($nestedMethodCall->name, 'getStatusCode')) {
-            return null;
-        }
-
-        // caller must be a response object
-        if (! $this->isObjectType($nestedMethodCall->var, new ObjectType(ResponseClass::BASIC))) {
-            return null;
-        }
-
-        $statusCode = $this->valueResolver->getValue($args[0]->value, true);
-
-        // handled by another method
-        if ($statusCode === 200) {
-            return null;
-        }
-
-        $newArgs = [$methodCall->args[0]];
-        // When we had a custom message argument we want to add it to the new assert.
-        if (isset($args[2])) {
-            if ($this->exprAnalyzer->isDynamicExpr($args[2]->value)) {
-                $newArgs[] = $args[2]->value;
-            } else {
-                $newArgs[] = $this->valueResolver->getValue($args[2]->value, true);
-            }
-        }
-
-        if ($methodCall instanceof StaticCall) {
-            return $this->nodeFactory->createStaticCall('self', 'assertResponseStatusCodeSame', $newArgs);
-        }
-
-        return $this->nodeFactory->createLocalMethodCall('assertResponseStatusCodeSame', $newArgs);
-    }
-
-    private function processAssertResponseRedirects(MethodCall|StaticCall $methodCall): MethodCall|StaticCall|null
-    {
-        if (! $this->testsNodeAnalyzer->isPHPUnitMethodCallNames($methodCall, ['assertSame'])) {
-            return null;
-        }
-
-        $args = $methodCall->getArgs();
-
-        $firstArgValue = $args[1]->value;
-        if (! $this->isGetLocationMethodCall($firstArgValue)) {
-            return null;
-        }
-
-        $newArgs = [$methodCall->args[0]];
-        if (isset($args[2])) {
-            // When we had a $message argument we want to add it to the new assert together with $expectedCode null.
-            $newArgs[] = null;
-            if ($this->exprAnalyzer->isDynamicExpr($args[2]->value)) {
-                $newArgs[] = $args[2]->value;
-            } else {
-                $newArgs[] = $this->valueResolver->getValue($args[2]->value, true);
-            }
-        }
-
-        if ($methodCall instanceof StaticCall) {
-            return $this->nodeFactory->createStaticCall('self', 'assertResponseRedirects', $newArgs);
-        }
-
-        return $this->nodeFactory->createLocalMethodCall('assertResponseRedirects', $newArgs);
     }
 }
