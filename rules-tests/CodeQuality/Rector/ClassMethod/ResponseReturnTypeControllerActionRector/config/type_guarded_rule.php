a024593e420e6f6082257546c09b54a487d6973d
Tomas Votruba <tomas.vot@gmail.com>
2026-07-21 11:18:32 +0200

    Use custom controller class in typeGuardedClasses test fixtures

--- rules-tests/CodeQuality/Rector/ClassMethod/ResponseReturnTypeControllerActionRector/config/type_guarded_rule.php
+++ rules-tests/CodeQuality/Rector/ClassMethod/ResponseReturnTypeControllerActionRector/config/type_guarded_rule.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Symfony\CodeQuality\Rector\ClassMethod\ResponseReturnTypeControllerActionRector;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Rector\Symfony\Tests\CodeQuality\Rector\ClassMethod\ResponseReturnTypeControllerActionRector\Source\AbstractCustomController;
 
 return RectorConfig::configure()
     ->withRules([ResponseReturnTypeControllerActionRector::class])
-    ->withTypeGuardedClasses([AbstractController::class]);
+    ->withTypeGuardedClasses([AbstractCustomController::class]);
