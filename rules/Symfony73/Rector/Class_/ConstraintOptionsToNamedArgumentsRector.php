696b1a4342607e544bcc5feea0665f3121c697ca
Tomas Votruba <tomas.vot@gmail.com>
2026-07-22 23:36:09 +0200

    [Symfony73] Wrap scalar groups option in array in ConstraintOptionsToNamedArgumentsRector

--- rules/Symfony73/Rector/Class_/ConstraintOptionsToNamedArgumentsRector.php
+++ rules/Symfony73/Rector/Class_/ConstraintOptionsToNamedArgumentsRector.php
@@ -162,7 +162,14 @@ CODE_SAMPLE
                 }
             }
 
-            $arg = new Arg($item->value);
+            $argValue = $item->value;
+
+            // the "groups" constructor argument is typed as array, so a scalar option must be wrapped
+            if ($keyValue === 'groups' && ! $argValue instanceof Array_) {
+                $argValue = new Array_([new ArrayItem($argValue)]);
+            }
+
+            $arg = new Arg($argValue);
             $arg->name = new Identifier($keyValue);
 
             $namedArgs[] = $arg;
