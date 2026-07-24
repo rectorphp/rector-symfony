17271b148fa5cdeb19e080a7be2e0dfe06df3f06
Tomas Votruba <tomas.vot@gmail.com>
2026-07-24 11:14:56 +0200

    Remove StructArmed
    Drop boundwize/structarmed dev dependency, its config, and CI step.

--- structarmed.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Boundwize\StructArmed\Architecture;
-use Boundwize\StructArmed\Preset\Preset;
-
-return Architecture::define()
-    ->withPreset(Preset::PSR4());
