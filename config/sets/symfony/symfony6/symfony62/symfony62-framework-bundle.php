73bd2df817de8ad0347ce63aadceca36d2a9eb9d
Tomas Votruba <tomas.vot@gmail.com>
2026-07-30 10:39:00 +0200

    [Deprecation] Deprecate SimplifyFormRenderingRector (#975)

--- config/sets/symfony/symfony6/symfony62/symfony62-framework-bundle.php
+++ config/sets/symfony/symfony6/symfony62/symfony62-framework-bundle.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Renaming\ValueObject\MethodCallRename;
-use Rector\Symfony\Symfony62\Rector\MethodCall\SimplifyFormRenderingRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(SimplifyFormRenderingRector::class);
-
     $rectorConfig->ruleWithConfiguration(
         RenameMethodRector::class,
         [
