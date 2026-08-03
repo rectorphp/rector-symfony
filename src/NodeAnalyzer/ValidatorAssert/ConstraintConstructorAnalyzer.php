df48f95734b40491d68d0c6199ee59588ae1a657
Tomas Votruba <tomas.vot@gmail.com>
2026-08-03 23:23:54 +0200

    [CodeQuality] Skip constraint without own constructor in LoadValidatorMetadataToAttributeRector (#1001)

---
+++ src/NodeAnalyzer/ValidatorAssert/ConstraintConstructorAnalyzer.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\NodeAnalyzer\ValidatorAssert;
+
+use PHPStan\Reflection\ReflectionProvider;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * A constraint that adds no constructor of its own inherits the one from Symfony\Component\Validator\Constraint,
+ * which takes the options array as a whole. Its options are plain properties, so there are no named arguments to
+ * move them to.
+ */
+final readonly class ConstraintConstructorAnalyzer
+{
+    public function __construct(
+        private ReflectionProvider $reflectionProvider,
+    ) {
+    }
+
+    public function hasOwnConstructor(string $constraintClass): bool
+    {
+        if (! $this->reflectionProvider->hasClass($constraintClass)) {
+            return false;
+        }
+
+        $classReflection = $this->reflectionProvider->getClass($constraintClass);
+        if (! $classReflection->hasConstructor()) {
+            return false;
+        }
+
+        $extendedMethodReflection = $classReflection->getConstructor();
+
+        return $extendedMethodReflection->getDeclaringClass()
+            ->getName() !== Constraint::class;
+    }
+}
