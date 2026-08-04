6c7e215c378ed4cad1b6268c9e56245a6c268d1f
Tomas Votruba <tomas.vot@gmail.com>
2026-08-04 12:27:55 +0200

    [CodeQuality] Skip type-guarded classes + unique autowire method name in ControllerMethodInjectionToConstructorRector (#1007)

---
+++ rules-tests/CodeQuality/Rector/Class_/ControllerMethodInjectionToConstructorRector/Source/AbstractCustomController.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Tests\CodeQuality\Rector\Class_\ControllerMethodInjectionToConstructorRector\Source;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+abstract class AbstractCustomController extends AbstractController
+{
+}
