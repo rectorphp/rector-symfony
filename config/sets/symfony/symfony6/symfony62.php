42a55ca7ced1499dd05d836b063d2089ff06ce4f
Tomas Votruba <tomas.vot@gmail.com>
2026-08-07 12:02:07 +0200

    [Symfony27] Move the 2.7 configuration to the composer-based set
    The "setDefaultOptions" rename is bound to symfony/form 2.7 and the
    "OptionsResolverInterface" rename to symfony/options-resolver 2.7,
    so the symfony27 set has nothing left to register.

--- config/sets/symfony/symfony6/symfony62.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/symfony62/symfony62-console.php');
-};
