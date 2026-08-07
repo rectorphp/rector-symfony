337d48ed3c806cfdca8e73e71bfda71f9d212e26
Tomas Votruba <tomas.vot@gmail.com>
2026-08-07 14:03:03 +0200

    [Sets] Convert the last version sets to the composer-based set
    The remaining Symfony version sets are moved to "composer-based.php",
    bound to the package version their target API changed in:
    
    * symfony28 -> symfony/routing >=2.8
    * symfony30-bridge-monolog -> symfony/monolog-bridge >=3.0
    * symfony50-types -> symfony/console, doctrine-bridge, event-dispatcher,
      form, process, security-core and translation-contracts >=5.0
    * symfony62-console -> symfony/console >=6.2
    
    The Twig version sets are dropped as well, as every one of them is
    already covered by the Twig "composer-based.php".

--- src/Set/TwigSetList.php
+++ src/Set/TwigSetList.php
@@ -10,20 +10,4 @@ namespace Rector\Symfony\Set;
 final class TwigSetList
 {
     public const string COMPOSER_BASED = __DIR__ . '/../../config/sets/twig/composer-based.php';
-
-    public const string TWIG_112 = __DIR__ . '/../../config/sets/twig/twig112.php';
-
-    public const string TWIG_127 = __DIR__ . '/../../config/sets/twig/twig127.php';
-
-    public const string TWIG_134 = __DIR__ . '/../../config/sets/twig/twig134.php';
-
-    public const string TWIG_140 = __DIR__ . '/../../config/sets/twig/twig140.php';
-
-    public const string TWIG_20 = __DIR__ . '/../../config/sets/twig/twig20.php';
-
-    public const string TWIG_24 = __DIR__ . '/../../config/sets/twig/twig24.php';
-
-    public const string TWIG_30 = __DIR__ . '/../../config/sets/twig/twig30.php';
-
-    public const string TWIG_UNDERSCORE_TO_NAMESPACE = __DIR__ . '/../../config/sets/twig/twig-underscore-to-namespace.php';
 }
