80bd1ac194b7a8df02530e2b85d172350b021d15
Tomas Votruba <tomas.vot@gmail.com>
2026-08-11 11:50:11 +0200

    [Symfony 7.3] Restore GetFilters/GetFunctions to AsTwig attribute rules as deprecated
    Point users to GetFiltersAndFunctionsToAsTwigAttributeRector instead.

---
+++ rules/Symfony73/Rector/Class_/GetFiltersToAsTwigFilterAttributeRector.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony\Symfony73\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use Rector\Configuration\Deprecation\Contract\DeprecatedInterface;
+use Rector\Exception\ShouldNotHappenException;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @deprecated Handling getFilters() alone leaves the sibling getFunctions()/getTests() methods behind and can produce
+ *             a half-converted extension. Use the GetFiltersAndFunctionsToAsTwigAttributeRector rule instead, that
+ *             converts all the get methods at once.
+ */
+final class GetFiltersToAsTwigFilterAttributeRector extends AbstractRector implements DeprecatedInterface
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Changes getFilters() in TwigExtension to #[AsTwigFilter] marker attribute above local class method',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+use Twig\Extension\AbstractExtension;
+use Twig\Environment;
+
+class SomeClass extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new \Twig\TwigFilter('filter_name', [$this, 'localMethod', 'needs_environment' => true]),
+        ];
+    }
+
+    public function localMethod(Environment $env, $value)
+    {
+        return $value;
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+use Twig\Extension\AbstractExtension;
+use Twig\Attribute\AsTwigFilter;
+use Twig\Environment;
+
+class SomeClass extends AbstractExtension
+{
+    #[AsTwigFilter('filter_name', needsEnvironment: true)]
+    public function localMethod(Environment $env, $value)
+    {
+        return $value;
+    }
+}
+CODE_SAMPLE
+                )]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Class_ $node
+     */
+    public function refactor(Node $node): ?Class_
+    {
+        throw new ShouldNotHappenException(sprintf(
+            '"%s" is deprecated, as it converts getFilters() only and leaves getFunctions()/getTests() behind. Use "%s" instead.',
+            self::class,
+            GetFiltersAndFunctionsToAsTwigAttributeRector::class
+        ));
+    }
+}
