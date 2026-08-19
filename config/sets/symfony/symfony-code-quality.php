6b771123f3327ac85ec55be56413ba69ade37069
Tomas Votruba <tomas.vot@gmail.com>
2026-08-19 21:26:05 +0100

    [CodeQuality] Move version-bonded rules to composer-based set
    SingleConditionSecurityAttributeToIsGrantedRector, SplitAndSecurityAttributeToIsGrantedRector,
    RedirectToRouteRector and ParameterBagTypedGetMethodCallRector are version-bonded and belong in
    composer-based.php under their Symfony version. Drop their duplicate registration from
    symfony-code-quality.php. ParameterBagTypedGetMethodCallRector now implements
    ComposerPackageConstraintInterface (symfony/http-foundation >=5.1).

--- config/sets/symfony/symfony-code-quality.php
+++ config/sets/symfony/symfony-code-quality.php
@@ -14,7 +14,6 @@ use Rector\Symfony\CodeQuality\Rector\ClassMethod\RemoveUnusedRequestParamRector
 use Rector\Symfony\CodeQuality\Rector\ClassMethod\ResponseReturnTypeControllerActionRector;
 use Rector\Symfony\CodeQuality\Rector\MethodCall\AssertSameResponseCodeWithDebugContentsRector;
 use Rector\Symfony\CodeQuality\Rector\MethodCall\LiteralGetToRequestClassConstantRector;
-use Rector\Symfony\CodeQuality\Rector\MethodCall\ParameterBagTypedGetMethodCallRector;
 use Rector\Symfony\CodeQuality\Rector\MethodCall\StringCastDebugResponseRector;
 use Rector\Symfony\CodeQuality\Rector\Trait_\AddTraitGetterReturnTypeBasedOnSetterRequiredRector;
 
@@ -35,7 +34,6 @@ return static function (RectorConfig $rectorConfig): void {
 
         // request method
         RequestIsMainRector::class,
-        ParameterBagTypedGetMethodCallRector::class,
 
         // tests
         AssertSameResponseCodeWithDebugContentsRector::class,
