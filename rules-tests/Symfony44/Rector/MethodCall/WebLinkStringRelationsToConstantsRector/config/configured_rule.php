556739f78edf8e312530e3b0cbd19619c1d9f91e
Johan Vlaar <JohJohan@users.noreply.github.com>
2026-08-26 11:02:27 +0200

    [Web Link] add WebLinkStringRelationsToConstantsRector (#1058)

---
+++ rules-tests/Symfony44/Rector/MethodCall/WebLinkStringRelationsToConstantsRector/config/configured_rule.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Symfony\Symfony44\Rector\MethodCall\WebLinkStringRelationsToConstantsRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(WebLinkStringRelationsToConstantsRector::class);
+};
